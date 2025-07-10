4e2d1c048224cc5269a8572ca540b9cdf4c36561
praisebak <60121346+praisebak@users.noreply.github.com>
2025-07-10 21:40:51 +0900

    [BE] 백엔드 프로젝트 초기 세팅 (#7)

---
+++ server/src/main/java/com/ahmadda/AhMaddaApplication.java
@@ -0,0 +1,13 @@
+package com.ahmadda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhMaddaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhMaddaApplication.class, args);
+    }
+
+}
