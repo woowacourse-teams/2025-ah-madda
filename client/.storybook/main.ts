b6031ec2775cef31c89ef07bded783de2cc04822
yeji0214 <1225yj50@naver.com>
2025-07-14 21:59:45 +0900

    feat: alias 설정 추가

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,18 +1,33 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
   ],
-  "addons": [
-    "@storybook/addon-webpack5-compiler-swc",
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
-  ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...(config.resolve?.alias || {}),
+          '@': path.resolve(__dirname, '../src'),
+        },
+      },
+    };
+  },
 };
+
 export default config;
