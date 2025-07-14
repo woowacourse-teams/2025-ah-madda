05567ce108866f318260259b548139c02ddece3d
장정안 <wjddks96@naver.com>
2025-07-14 17:58:21 +0900

    chore: test 시 alias 설정

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,8 +1,16 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTest.ts'],
     include: ['src/__test__/*.test.ts?(x)'],
     exclude: [
       '**/node_modules/**',
