253526a749f25830d077fb75310a16a4a6e90147
jeyong <joon6093@naver.com>
2025-07-15 16:10:44 +0900

    refactor: NotFoundException의 상속 구조 변경

--- server/src/main/java/com/ahmadda/application/exception/NotFoundException.java
+++ server/src/main/java/com/ahmadda/application/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package com.ahmadda.application.exception;
 
-public class NotFoundException extends RuntimeException {
+public class NotFoundException extends BusinessFlowViolatedException {
 
     public NotFoundException(final String message) {
         super(message);
