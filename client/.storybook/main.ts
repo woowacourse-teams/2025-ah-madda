01353f98b38d7eb8b3d00d932eadc96dadbd55fe
keemsebeen <sebin0580@gmail.com>
2025-07-16 15:50:17 +0900

    [FE] Button 컴포넌트, 스토리북 구현 (#60)

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -6,6 +6,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
