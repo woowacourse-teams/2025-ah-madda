aef01cfe3802bd44c465380217962af57d353371
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-07-16 13:15:24 +0900

    refactor: 도메인에서 검증하도록 수정
    기존 dto에서 진행하던 검증을 도메인에서 하도록 옮겼습니다.

--- server/src/main/java/com/ahmadda/presentation/OrganizationCreateRequest.java
+++ server/src/main/java/com/ahmadda/presentation/OrganizationCreateRequest.java
@@ -2,10 +2,9 @@ package com.ahmadda.presentation;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
 
-public record OrganizationCreateRequest(@NotBlank @NotNull @Length(min = 2, max = 20) String name,
-                                        @NotBlank @NotNull @Length(min = 2, max = 2000) String description,
+public record OrganizationCreateRequest(@NotBlank @NotNull String name,
+                                        @NotBlank @NotNull String description,
                                         @NotNull String imageUrl) {
 
 }
