66bdc6d26a97a16becb78e090cd998d854232188
keemsebeen <sebin0580@gmail.com>
2025-07-16 21:39:09 +0900

    design: Card 컴포넌트 style 설정

---
+++ client/src/shared/components/Card/Card.styled.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const StyledCard = styled.div`
+  display: block;
+  width: 100%;
+  padding: 1.375rem;
+  box-sizing: border-box;
+  border-radius: 0.875rem;
+  border: 1px solid #e5e7eb;
+  background-color: #ffffff;
+`;
