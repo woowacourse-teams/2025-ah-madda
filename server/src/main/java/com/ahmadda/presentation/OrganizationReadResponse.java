ac1fc94e207a0376b73e277f06039484bbbd070f
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-07-16 13:20:53 +0900

    refactor: organization read시 id를 반환하도록 수정

--- server/src/main/java/com/ahmadda/presentation/OrganizationReadResponse.java
+++ server/src/main/java/com/ahmadda/presentation/OrganizationReadResponse.java
@@ -2,10 +2,11 @@ package com.ahmadda.presentation;
 
 import com.ahmadda.domain.Organization;
 
-public record OrganizationReadResponse(String name, String description, String imageUrl) {
+public record OrganizationReadResponse(Long id, String name, String description, String imageUrl) {
 
     public static OrganizationReadResponse from(final Organization organization) {
-        return new OrganizationReadResponse(organization.getName(),
+        return new OrganizationReadResponse(organization.getId(),
+                                            organization.getName(),
                                             organization.getDescription(),
                                             organization.getImageUrl()
         );
