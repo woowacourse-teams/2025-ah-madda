09908f5cb9d4736e6f7e7db3d28ae738f9ce00f3
yeji0214 <1225yj50@naver.com>
2025-07-16 23:08:48 +0900

    design: padding 및 모바일 대응 스타일 수정

--- client/src/shared/components/Header/Header.styled.ts
+++ client/src/shared/components/Header/Header.styled.ts
@@ -3,12 +3,16 @@ import styled from '@emotion/styled';
 export const StyledHeader = styled.header<{ justifyContent: string }>`
   position: sticky;
   top: 0;
-  height: 30px;
-  padding: 16px;
+  height: 60px;
+  padding: 0 60px;
   display: flex;
   align-items: center;
   justify-content: ${({ justifyContent }) => justifyContent};
   flex-shrink: 0;
   z-index: 1;
   border-bottom: 1px solid #e5e5e5;
+
+  @media (max-width: 768px) {
+    padding: 0 20px;
+  }
 `;
