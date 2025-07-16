9cc0b4e94574a473c5df1ee2644819806ef2bd1c
장정안 <wjddks96@naver.com>
2025-07-16 16:36:05 +0900

    refactor: 자주 쓰이는 prop만 담당하게 리팩토링

--- client/src/shared/components/Flex/Flex.styled.ts
+++ client/src/shared/components/Flex/Flex.styled.ts
@@ -1,145 +1,30 @@
 import styled from '@emotion/styled';
 
-export type FlexStyle = {
-  direction?: 'row' | 'column';
-
-  align?:
-    | 'normal'
-    | 'normal'
-    | 'stretch'
-    | 'center'
-    | 'start'
-    | 'end'
+type StyledFlexProps = {
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  justifyContent?:
     | 'flex-start'
     | 'flex-end'
-    | 'self-start'
-    | 'self-end'
-    | 'baseline'
-    | 'inherit'
-    | 'initial'
-    | 'unset';
-
-  justify?:
     | 'center'
-    | 'start'
-    | 'flex-start'
-    | 'end'
-    | 'flex-end'
-    | 'left'
-    | 'right'
-    | 'normal'
     | 'space-between'
     | 'space-around'
-    | 'space-evenly'
-    | 'stretch'
-    | 'inherit'
-    | 'initial'
-    | 'revert'
-    | 'unset';
-
-  gap?: string;
-
-  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse' | 'inherit' | 'initial';
-
-  basis?: 0 | 'auto' | '200px';
-
-  grow?: string;
-
-  shrink?: number;
-
-  position?: 'static' | 'absolute' | 'relative' | 'fixed' | 'sticky' | 'inherit';
-
-  top?: string;
-
-  right?: string;
-
-  bottom?: string;
-
-  left?: string;
-
+    | 'space-evenly';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+  gap?: number | string;
+  margin?: number | string;
+  padding?: number | string;
   width?: string;
-
   height?: string;
-
-  maxWidth?: string;
-
-  maxHeight?: string;
-
-  margin?: string;
-
-  marginRight?: string;
-
-  marginTop?: string;
-
-  marginLeft?: string;
-
-  marginBottom?: string;
-
-  padding?: string;
-
-  paddingTop?: string;
-
-  paddingRight?: string;
-
-  paddingBottom?: string;
-
-  paddingLeft?: string;
 };
 
-export const StyledFlex = styled.div<FlexStyle>`
+export const StyledFlexBox = styled.div<StyledFlexProps>`
   display: flex;
-
-  position: ${({ position = 'static' }) => position};
-
-  top: ${({ top = '' }) => top};
-
-  right: ${({ right = '' }) => right};
-
-  bottom: ${({ bottom = '' }) => bottom};
-
-  left: ${({ left = '' }) => left};
-
-  flex-wrap: ${({ wrap = 'nowrap' }) => wrap};
-
-  flex-basis: ${({ basis = 'auto' }) => basis};
-
-  flex-grow: ${({ grow = '0' }) => grow};
-
-  flex-shrink: ${({ shrink = 0 }) => shrink};
-
-  flex-direction: ${({ direction = 'row' }) => direction};
-
-  align-items: ${({ align = 'flex-start' }) => align};
-
-  justify-content: ${({ justify = 'flex-start' }) => justify};
-
-  gap: ${({ gap = '0px' }) => gap};
-
-  width: ${({ width = '' }) => width};
-
-  height: ${({ height = '' }) => height};
-
-  max-width: ${({ maxWidth = '' }) => maxWidth};
-
-  max-height: ${({ maxHeight = '' }) => maxHeight};
-
-  margin: ${({ margin = '0' }) => margin};
-
-  margin-right: ${({ marginRight = '' }) => marginRight};
-
-  margin-left: ${({ marginLeft = '' }) => marginLeft};
-
-  margin-top: ${({ marginTop = '' }) => marginTop};
-
-  margin-bottom: ${({ marginBottom = '' }) => marginBottom};
-
-  padding: ${({ padding = '' }) => padding};
-
-  padding-top: ${({ paddingTop = '' }) => paddingTop};
-
-  padding-right: ${({ paddingRight = '' }) => paddingRight};
-
-  padding-bottom: ${({ paddingBottom = '' }) => paddingBottom};
-
-  padding-left: ${({ paddingLeft = '' }) => paddingLeft};
+  flex-direction: ${({ direction }) => direction};
+  justify-content: ${({ justifyContent }) => justifyContent};
+  align-items: ${({ alignItems }) => alignItems};
+  gap: ${({ gap = 0 }) => (typeof gap === 'number' ? `${gap}px` : gap)};
+  margin: ${({ margin = 0 }) => (typeof margin === 'number' ? `${margin}px` : margin)};
+  padding: ${({ padding = 0 }) => (typeof padding === 'number' ? `${padding}px` : padding)};
+  width: ${({ width = 'auto' }) => width};
+  height: ${({ height = 'auto' }) => height};
 `;
