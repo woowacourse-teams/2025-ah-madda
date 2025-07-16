ce01f18fc3424592aaba501ee986a9fd701762b4
keemsebeen <sebin0580@gmail.com>
2025-07-16 12:37:07 +0900

    feat: Button 컴포넌트 storybook 구현

--- client/src/shared/components/Button/Button.stories.tsx
+++ client/src/shared/components/Button/Button.stories.tsx
@@ -9,11 +9,36 @@ const meta = {
 } satisfies Meta<typeof Button>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>;
 
 export const Basic: Story = {
   args: {
-    children: '버튼123',
+    children: 'Button',
+    width: '8rem',
+    size: 'md',
+    variant: 'filled',
+    color: '#2563EB',
+    fontColor: 'white',
+  },
+  render: (args) => <Button {...args} />,
+};
+
+export const Outlined: Story = {
+  args: {
+    width: '10rem',
+    variant: 'outlined',
+    color: '#808080',
+    fontColor: '#000000',
+    children: 'Outlined Button',
+  },
+  render: (args) => <Button {...args} />,
+};
+
+export const FullWidth: Story = {
+  args: {
+    children: 'Full Width Button',
+    width: '100%',
+    color: '#409869',
   },
   render: (args) => <Button {...args} />,
 };
