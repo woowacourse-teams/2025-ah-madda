57511317f0436bebae10cfbf19e76234cc66471b
yeji0214 <1225yj50@naver.com>
2025-07-17 15:25:34 +0900

    design: Modal 컴포넌트 스타일 수정

--- client/src/shared/components/Modal/Modal.styled.ts
+++ client/src/shared/components/Modal/Modal.styled.ts
@@ -7,19 +7,28 @@ export type Size = 'small' | 'medium' | 'large';
 export const positionStyles = {
   center: css`
     align-items: center;
-    border-radius: 8px;
   `,
   top: css`
     align-items: flex-start;
-    border-radius: 0 0 8px 8px;
   `,
   bottom: css`
     align-items: flex-end;
+  `,
+} as const;
+
+export const radiusStyles = {
+  center: css`
+    border-radius: 8px;
+  `,
+  top: css`
+    border-radius: 0 0 8px 8px;
+  `,
+  bottom: css`
     border-radius: 8px 8px 0 0;
   `,
 } as const;
 
-export const sizeStyles = {
+const sizeStyles = {
   small: css`
     width: 304px;
   `,
@@ -31,31 +40,30 @@ export const sizeStyles = {
   `,
 } as const;
 
-export const StyledModalLayout = styled.div`
+export const StyledModalLayout = styled.div<{ position: Position }>`
   position: fixed;
   inset: 0;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
-  align-items: center;
+  ${({ position }) => positionStyles[position]};
   z-index: 1000;
 `;
 
-export const StyledModalContainer = styled.div<{ size: Size }>`
+export const StyledModalContainer = styled.div<{ size: Size; position: Position }>`
   background-color: #ffffff;
   min-height: 216px;
   padding: 24px 32px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  border-radius: 10px;
   position: relative;
+  ${({ position }) => radiusStyles[position]};
   ${({ size }) => sizeStyles[size]};
 `;
 
-export const StyledModalWrapper = styled.div<{ position: Position }>`
+export const StyledModalWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  ${({ position }) => positionStyles[position]};
 `;
 
 export const StyledCloseButtonWrapper = styled.div`
