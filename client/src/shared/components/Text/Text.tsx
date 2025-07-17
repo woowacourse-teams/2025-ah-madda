0e965fb8acd6549958df548b7504ecd4c32ecc28
keemsebeen <sebin0580@gmail.com>
2025-07-17 13:45:57 +0900

    fix: Text 컴포넌트 children type 수정

--- client/src/shared/components/Text/Text.tsx
+++ client/src/shared/components/Text/Text.tsx
@@ -30,7 +30,7 @@ export type TextProps<T extends TextElementType> = {
   /**
    * The content to be displayed inside the text component.
    */
-  children: React.ReactNode;
+  children: string | number;
 } & Omit<ComponentProps<T>, 'as' | 'children' | 'color'>;
 
 export const Text = <T extends TextElementType = 'p'>({
