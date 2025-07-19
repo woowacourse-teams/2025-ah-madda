206382bc8e4de92190da6204d2bdd567fef0c149
jumdo12 <jumdo12@gmail.com>
2025-07-19 14:44:05 +0900

    refactor: 불필요한 상수 제거

--- server/src/main/java/com/ahmadda/presentation/LoginController.java
+++ server/src/main/java/com/ahmadda/presentation/LoginController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LoginController {
 
-    public static final int COOKIE_AGE_SECONDS = 604800;
-
     private final LoginService loginService;
 
     @PostMapping("/login")
