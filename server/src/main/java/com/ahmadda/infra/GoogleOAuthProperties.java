ade79c76de927aa04d016b0843a9f5efe27b4108
jumdo12 <jumdo12@gmail.com>
2025-07-19 16:14:33 +0900

    refactor: Properties 생성자 개선

--- server/src/main/java/com/ahmadda/infra/GoogleOAuthProperties.java
+++ server/src/main/java/com/ahmadda/infra/GoogleOAuthProperties.java
@@ -2,16 +2,34 @@ package com.ahmadda.infra;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 @ConfigurationProperties(prefix = "google.oauth2")
 @Getter
 public class GoogleOAuthProperties {
 
-    private String clientId;
-    private String clientSecret;
-    private String redirectUri;
-    private String tokenUri;
-    private String userUri;
-    private int connectTimeout;
-    private int readTimeout;
+    private final String clientId;
+    private final String clientSecret;
+    private final String redirectUri;
+    private final String tokenUri;
+    private final String userUri;
+    private final int connectTimeout;
+    private final int readTimeout;
+
+    @ConstructorBinding
+    public GoogleOAuthProperties(String clientId,
+                                 String clientSecret,
+                                 String redirectUri,
+                                 String tokenUri,
+                                 String userUri,
+                                 int connectTimeout,
+                                 int readTimeout) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.redirectUri = redirectUri;
+        this.tokenUri = tokenUri;
+        this.userUri = userUri;
+        this.connectTimeout = connectTimeout;
+        this.readTimeout = readTimeout;
+    }
 }
