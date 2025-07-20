85a10dcf6fd20feadcaa3c9755aaac57952b256b
keemsebeen <sebin0580@gmail.com>
2025-07-21 03:59:51 +0900

    feat: 날짜별 컴포넌트 그룹화 util 함수 구현

---
+++ client/src/features/Event/utils/groupEventsByDate.ts
@@ -0,0 +1,38 @@
+import { Event } from '../types/Event';
+
+export const groupEventsByDate = (events: Event[]) => {
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const groups: { [key: string]: Event[] } = {
+    오늘: [],
+    내일: [],
+  };
+
+  events.forEach((event) => {
+    const eventDate = new Date(event.registrationEndDate);
+
+    if (eventDate.toDateString() === today.toDateString()) {
+      groups['오늘'].push(event);
+      return;
+    }
+    if (eventDate.toDateString() === tomorrow.toDateString()) {
+      groups['내일'].push(event);
+      return;
+    }
+
+    const dateKey = eventDate.toLocaleDateString('ko-KR', {
+      month: 'long',
+      day: 'numeric',
+    });
+
+    if (!groups[dateKey]) {
+      groups[dateKey] = [];
+    }
+
+    groups[dateKey].push(event);
+  });
+
+  return groups;
+};
