f7075e09ac11dbaa531f3399ccb786569b54af91
keemsebeen <sebin0580@gmail.com>
2025-07-21 04:00:10 +0900

    feat: EventPage 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,5 @@
-import { useState } from 'react';
-
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { Event } from './features/Event';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
-
-  const handleButtonClick = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <Container
-      css={css`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-      `}
-    >
-      <button onClick={handleButtonClick}>Click me</button>
-      <p>{count}</p>
-      <h1>React + TypeScript + Webpack</h1>
-    </Container>
-  );
+  return <Event />;
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`;
