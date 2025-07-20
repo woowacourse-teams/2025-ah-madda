575c22bc9bdde0bb9ccf3ae42c9a44712504e911
keemsebeen <sebin0580@gmail.com>
2025-07-21 03:42:58 +0900

    chore: App.tsx에 Outlet 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-// S.TODO: 추후 div -> AppLayout 컴포넌트로 변경 예정
 export const App = () => {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 };
