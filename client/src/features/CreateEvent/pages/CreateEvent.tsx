050163cfaf7e476aaa456db290fe2780ddc37d99
yeji0214 <1225yj50@naver.com>
2025-07-21 13:46:55 +0900

    feat: 헤더 추가

--- client/src/features/CreateEvent/pages/CreateEvent.tsx
+++ client/src/features/CreateEvent/pages/CreateEvent.tsx
@@ -1,20 +1,36 @@
 import { Flex } from '../../../shared/components/Flex';
+import { Header } from '../../../shared/components/Header';
+import { IconButton } from '../../../shared/components/IconButton';
+import { Text } from '../../../shared/components/Text';
 import { EventCreateForm } from '../components/EventCreateForm';
 
 export const CreateEvent = () => {
   return (
-    <Flex
-      dir="column"
-      css={{
-        width: '100%',
-        maxWidth: '784px',
-        margin: '0 auto',
-        padding: '28px 14px',
-        gap: '24px',
-        boxSizing: 'border-box',
-      }}
-    >
-      <EventCreateForm />
-    </Flex>
+    <>
+      <Header
+        left={
+          <Flex alignItems="center" gap="12px">
+            <IconButton name="back" size={14} />
+            <Text type="caption">돌아가기</Text>
+          </Flex>
+        }
+        css={{
+          backgroundColor: 'white',
+        }}
+      />
+      <Flex
+        dir="column"
+        css={{
+          width: '100%',
+          maxWidth: '784px',
+          margin: '0 auto',
+          padding: '28px 14px',
+          gap: '24px',
+          boxSizing: 'border-box',
+        }}
+      >
+        <EventCreateForm />
+      </Flex>
+    </>
   );
 };
