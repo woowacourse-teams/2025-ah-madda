284b1ba1a06bed8377109bf42283876d4c0f8ea1
jeyong <joon6093@naver.com>
2025-07-21 14:26:20 +0900

    feat: 알람 메일 로깅 기능 구현

---
+++ server/src/main/java/com/ahmadda/infra/config/MailConfig.java
@@ -0,0 +1,25 @@
+package com.ahmadda.infra.config;
+
+import com.ahmadda.domain.NotificationMailer;
+import com.ahmadda.infra.MockNotificationMailer;
+import com.ahmadda.infra.SmtpNotificationMailer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "mail.mock", havingValue = "true")
+    public NotificationMailer mockMailService() {
+        return new MockNotificationMailer();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "mail.mock", havingValue = "false", matchIfMissing = true)
+    public NotificationMailer mailServiceImpl(final JavaMailSender javaMailSender) {
+        return new SmtpNotificationMailer(javaMailSender);
+    }
+}
