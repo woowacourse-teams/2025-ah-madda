9f15bc0304552acc944e6e62d5a9f56086a1ec5e
keemsebeen <sebin0580@gmail.com>
2025-07-22 04:09:10 +0900

    [FE] 패칭/서버 상태 관리 라이브러리 설정 (#111)

---
+++ client/src/api/fetcher.ts
@@ -0,0 +1,37 @@
+import ky, { Options, ResponsePromise } from 'ky';
+
+const defaultOption: Options = {
+  retry: 0,
+  timeout: 30_000,
+};
+const API_BASE_URL = process.env.API_BASE_URL;
+
+export const instance = ky.create({
+  prefixUrl: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  hooks: {
+    beforeRequest: [() => {}],
+    afterResponse: [() => {}],
+  },
+
+  ...defaultOption,
+});
+
+export async function parseResponse<T>(response: ResponsePromise) {
+  return await response.json<T>();
+}
+
+export const fetcher = {
+  get: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(instance.get(pathname, options)),
+  post: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(instance.post(pathname, options)),
+  put: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(instance.put(pathname, options)),
+  delete: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(instance.delete(pathname, options)),
+  patch: <T>(pathname: string, options?: Options) =>
+    parseResponse<T>(instance.patch(pathname, options)),
+};
