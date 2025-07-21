19957a138b70b973becb9c6d52761ef42047fcf3
keemsebeen <sebin0580@gmail.com>
2025-07-21 16:05:26 +0900

    fix: EventContainer 컴포넌트 Flex 컴포넌트 적용

--- client/src/features/Event/containers/EventContainer.tsx
+++ client/src/features/Event/containers/EventContainer.tsx
@@ -1,20 +1,26 @@
 import { ReactNode } from 'react';
 
-import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+import { Flex } from '@/shared/components/Flex';
 
 type Props = {
   children: ReactNode;
 };
 export const EventContainer = ({ children }: Props) => {
-  return <Container>{children}</Container>;
+  return (
+    <Flex
+      as="main"
+      dir="column"
+      width="100%"
+      gap="40px"
+      padding="20px"
+      css={css`
+        background-color: rgba(231, 231, 231, 0.47);
+        border-radius: 10px;
+      `}
+    >
+      {children}
+    </Flex>
+  );
 };
-
-const Container = styled.main`
-  display: flex;
-  flex-direction: column;
-  border-radius: 10px;
-  gap: 40px;
-  width: 100%;
-  padding: 20px;
-  background-color: rgba(231, 231, 231, 0.47);
-`;
