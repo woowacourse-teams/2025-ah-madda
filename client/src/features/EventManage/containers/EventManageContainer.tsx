e22cbd69c17456f084430d0db41355ab50ebe8d5
장정안 <wjddks96@naver.com>
2025-07-21 17:08:05 +0900

    feat: Page Layout 설정

--- client/src/features/EventManage/containers/EventManageContainer.tsx
+++ client/src/features/EventManage/containers/EventManageContainer.tsx
@@ -1,6 +1,4 @@
 import { Flex } from '@/shared/components/Flex';
-import { Header } from '@/shared/components/Header';
-import { IconButton } from '@/shared/components/IconButton';
 import { Text } from '@/shared/components/Text';
 
 import { useEventManage } from '../hooks/useEventManage';
@@ -31,19 +29,5 @@ export const EventManageContainer = ({ children }: EventManageContainerProps) =>
       </Flex>
     );
   }
-  return (
-    <Flex dir="column">
-      <Header
-        left={
-          <Flex alignItems="center" gap="12px">
-            <IconButton name="back" size={14} />
-            <Text as="h1" type="Title" weight="semibold">
-              이벤트 관리
-            </Text>
-          </Flex>
-        }
-      />
-      {children}
-    </Flex>
-  );
+  return <Flex dir="column">{children}</Flex>;
 };
--- client/src/features/EventManage/pages/EventManage.tsx
+++ client/src/features/EventManage/pages/EventManage.tsx
@@ -1,4 +1,8 @@
 import { Flex } from '@/shared/components/Flex';
+import { Header } from '@/shared/components/Header';
+import { IconButton } from '@/shared/components/IconButton';
+import { PageLayout } from '@/shared/components/PageLayout';
+import { Text } from '@/shared/components/Text';
 
 import { EventInfoSection } from '../components/EventInfoSection';
 import { GuestManageSection } from '../components/GuestManageSection';
@@ -9,14 +13,29 @@ export const EventManage = () => {
   const { data } = useEventManage();
 
   return (
-    <EventManageContainer>
-      <Flex as="main" gap="40px" css={{ marginTop: '32px' }}>
-        <EventInfoSection eventInfo={data.eventInfo} />
-        <GuestManageSection
-          completedGuests={data.completedGuests}
-          pendingGuests={data.pendingGuests}
+    <PageLayout
+      header={
+        <Header
+          left={
+            <Flex alignItems="center" gap="12px">
+              <IconButton name="back" size={14} />
+              <Text as="h1" type="Title" weight="semibold">
+                이벤트 관리
+              </Text>
+            </Flex>
+          }
         />
-      </Flex>
-    </EventManageContainer>
+      }
+    >
+      <EventManageContainer>
+        <Flex as="main" gap="40px" css={{ marginTop: '32px' }} width="100%">
+          <EventInfoSection eventInfo={data.eventInfo} />
+          <GuestManageSection
+            completedGuests={data.completedGuests}
+            pendingGuests={data.pendingGuests}
+          />
+        </Flex>
+      </EventManageContainer>
+    </PageLayout>
   );
 };
