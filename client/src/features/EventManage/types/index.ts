c974023fe45dd4573363468e62a5f3f7524564cc
장정안 <wjddks96@naver.com>
2025-07-21 14:18:57 +0900

    chore: 타입 주석 제거

--- client/src/features/EventManage/types/index.ts
+++ client/src/features/EventManage/types/index.ts
@@ -2,13 +2,13 @@ export type EventInfo = {
   id: string;
   title: string;
   description: string;
-  organizer: string; // 주최자
+  organizer: string;
   location: string;
-  deadlineTime: string; // 신청 마감
-  startTime: string; // 이벤트 시작 시간
-  endTime: string; // 이벤트 종료 시간
-  currentParticipants: number; // 현재 참가자 수
-  maxParticipants: number; // 최대 참가자 수
+  deadlineTime: string;
+  startTime: string;
+  endTime: string;
+  currentParticipants: number;
+  maxParticipants: number;
 };
 
 export type Guest = {
