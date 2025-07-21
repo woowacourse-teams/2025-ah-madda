dd2689f3cf764c5e88b9c7abd618c1a6d9ef2ee1
jumdo12 <jumdo12@gmail.com>
2025-07-21 13:43:00 +0900

    refactor: 생성자 불일치 개선

--- server/src/main/java/com/ahmadda/infra/config/JwtTokenProperties.java
+++ server/src/main/java/com/ahmadda/infra/config/JwtTokenProperties.java
@@ -1,15 +1,19 @@
 package com.ahmadda.infra.config;
 
+import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.nio.charset.StandardCharsets;
 import javax.crypto.SecretKey;
 
 @ConfigurationProperties(prefix = "jwt")
-@RequiredArgsConstructor
 @Getter
 public class JwtTokenProperties {
 
     private final SecretKey secretKey;
+
+    public JwtTokenProperties(final String secretKey) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
 }
