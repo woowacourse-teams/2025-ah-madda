65426d914b94272b81b31f17e8fd1a29ac7d1150
keemsebeen <sebin0580@gmail.com>
2025-07-22 14:27:47 +0900

    feat: 이벤트 전체 조회 페이지 내 이벤트 생성 버튼 추가

---
+++ client/src/features/Event/Overview/components/EventList.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/shared/components/Button';
+import { Flex } from '@/shared/components/Flex';
+
+import { EventContainer } from '../containers/EventContainer';
+import { eventMock } from '../fixtures/mock';
+import { groupEventsByDate } from '../utils/groupEventsByDate';
+
+import { EventCard } from './EventCard';
+import { EventSection } from './EventSection';
+
+export const EventList = () => {
+  const groupedEvents = groupEventsByDate(eventMock);
+  const navigate = useNavigate();
+
+  return (
+    <EventContainer>
+      <Flex justifyContent="flex-end" alignItems="center">
+        <Button width="130px" size="md" onClick={() => navigate('/event/new')}>
+          + 이벤트 생성
+        </Button>
+      </Flex>
+      {Object.entries(groupedEvents).map(
+        ([dateTitle, events]) =>
+          events.length > 0 && (
+            <EventSection key={dateTitle} title={dateTitle}>
+              <EventGrid>
+                {events.map((event, index) => (
+                  <EventCard key={index} {...event} />
+                ))}
+              </EventGrid>
+            </EventSection>
+          )
+      )}
+    </EventContainer>
+  );
+};
+
+export const EventGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1.5rem;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
