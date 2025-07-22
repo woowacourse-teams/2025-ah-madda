5890721a0277681da94d7cba65aa9ae480097e8a
keemsebeen <sebin0580@gmail.com>
2025-07-22 15:20:19 +0900

    design: OrganizationInfo 컴포넌트 max-width 지정

--- client/src/features/Event/Overview/components/OrganizationInfo.tsx
+++ client/src/features/Event/Overview/components/OrganizationInfo.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import Wowaw from '@/assets/icon/wowaw.png';
@@ -6,15 +7,25 @@ import { Text } from '@/shared/components/Text';
 
 export const OrganizationInfo = () => {
   return (
-    <Flex padding="20px 10px" justifyContent="space-between" alignItems="center" width="100%">
-      <Flex dir="column" gap="8px">
-        <Text type="Head" weight="bold">
-          우아한테크코스 7기
-        </Text>
-        <Text type="Body">우아한테크코스의 이벤트를 모두 받아보세요.</Text>
-        <Text type="caption">6개의 이벤트가 열려있어요!</Text>
+    <Flex
+      dir="column"
+      width="100%"
+      gap="20px"
+      css={css`
+        max-width: 1120px;
+        margin: 0 auto;
+      `}
+    >
+      <Flex padding="20px 10px" justifyContent="space-between" alignItems="center" width="100%">
+        <Flex dir="column" gap="8px">
+          <Text type="Head" weight="bold">
+            우아한테크코스 7기
+          </Text>
+          <Text type="Body">우아한테크코스의 이벤트를 모두 받아보세요.</Text>
+          <Text type="caption">6개의 이벤트가 열려있어요!</Text>
+        </Flex>
+        <Img src={Wowaw} />
       </Flex>
-      <Img src={Wowaw} />
     </Flex>
   );
 };
