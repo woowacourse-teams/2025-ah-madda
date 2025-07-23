1c7de4579ab8d30fb48b25523ae1eecdf401b43e
yeji0214 <1225yj50@naver.com>
2025-07-23 20:16:21 +0900

    feat: createEventAPI 함수 구현

---
+++ client/src/api/queries/event.ts
@@ -0,0 +1,27 @@
+import { queryOptions } from '@tanstack/react-query';
+
+import { CreateEventRequest, EventDetail } from '../../features/Event/types/Event';
+import { fetcher } from '../fetcher';
+
+export const eventQueryKeys = {
+  all: () => ['event'],
+  detail: (eventId: number) => [...eventQueryKeys.all(), 'detail', eventId],
+};
+
+export const eventQueryOptions = {
+  detail: (eventId: number) =>
+    queryOptions({
+      queryKey: eventQueryKeys.detail(eventId),
+      queryFn: () => getEventDetailAPI(eventId),
+    }),
+};
+
+const createEventAPI = (organizationId: number, data: CreateEventRequest) => {
+  return fetcher.post<{ eventId: number }>(`organizations/${organizationId}/events`, {
+    json: data,
+  });
+};
+
+const getEventDetailAPI = (eventId: number) => {
+  return fetcher.get<EventDetail>(`organizations/events/${eventId}`);
+};
