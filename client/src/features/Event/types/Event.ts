d71b6dfb3b3287ce725f7067f726f11561486d4f
yeji0214 <1225yj50@naver.com>
2025-07-23 21:10:33 +0900

    refactor: Questions 타입 분리

--- client/src/features/Event/types/Event.ts
+++ client/src/features/Event/types/Event.ts
@@ -24,12 +24,14 @@ export type EventDetail = {
   registrationEnd: string;
   currentGuestCount: number;
   maxCapacity: number;
-  questions: {
-    questionId: number;
-    questionText: string;
-    isRequired: boolean;
-    orderIndex: number;
-  }[];
+  questions: Questions;
+};
+
+type Questions = {
+  questionId: number;
+  questionText: string;
+  isRequired: boolean;
+  orderIndex: number;
 };
 
 // S.TODO: 추후 Organization 폴더가 생기면 이동
