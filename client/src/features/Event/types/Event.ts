012ad766ee9ec8425d106b94ac113f4debd7252b
yeji0214 <1225yj50@naver.com>
2025-07-23 20:16:01 +0900

    feat: CreateEventRequest 타입 추가

--- client/src/features/Event/types/Event.ts
+++ client/src/features/Event/types/Event.ts
@@ -12,6 +12,43 @@ export type Event = {
   organizerName: string;
 };
 
+export type EventDetail = {
+  eventId: number;
+  title: string;
+  description: string;
+  place: string;
+  organizerName: string;
+  eventStart: string;
+  eventEnd: string;
+  registrationStart: string;
+  registrationEnd: string;
+  currentGuestCount: number;
+  maxCapacity: number;
+  questions: {
+    questionId: number;
+    questionText: string;
+    isRequired: boolean;
+    orderIndex: number;
+  }[];
+};
+
+export type CreateEventRequest = {
+  title: string;
+  description: string;
+  place: string;
+  maxCapacity: number;
+  eventStart: string;
+  eventEnd: string;
+  registrationStart: string;
+  registrationEnd: string;
+  organizerNickname: string;
+  questions: {
+    questionText: string;
+    isRequired: boolean;
+    orderIndex: number;
+  }[];
+};
+
 // S.TODO: 추후 Organization 폴더가 생기면 이동
 export type Organization = {
   organizationId: number;
