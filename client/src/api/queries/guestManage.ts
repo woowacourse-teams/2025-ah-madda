406d5fb98d819d71f3a323191c40df137b1d090f
장정안 <wjddks96@naver.com>
2025-07-23 22:41:45 +0900

    refactor: 쿼리키 컨벤션에 맞게 수정

--- client/src/api/queries/guestManage.ts
+++ client/src/api/queries/guestManage.ts
@@ -4,17 +4,17 @@ import { fetcher } from '../fetcher';
 
 export const guestManageQueryKeys = {
   all: () => ['guestManage'],
-  guests: (eventId: number) => [...guestManageQueryKeys.all(), 'guests', eventId],
-  nonGuests: (eventId: number) => [...guestManageQueryKeys.all(), 'nonGuests', eventId],
+  guests: () => [...guestManageQueryKeys.all(), 'guests'],
+  nonGuests: () => [...guestManageQueryKeys.all(), 'nonGuests'],
 };
 
 export const guestManageQueryOptions = {
   guests: (eventId: number) => ({
-    queryKey: guestManageQueryKeys.guests(eventId),
+    queryKey: [...guestManageQueryKeys.guests(), eventId],
     queryFn: () => getGuests(eventId),
   }),
   nonGuests: (eventId: number) => ({
-    queryKey: guestManageQueryKeys.nonGuests(eventId),
+    queryKey: [...guestManageQueryKeys.nonGuests(), eventId],
     queryFn: () => getNonGuests(eventId),
   }),
 };
