e7367551124fb061fea407726edc23119c9dc631
yeji0214 <1225yj50@naver.com>
2025-07-23 14:20:59 +0900

    feat: EventDetail 타입 추가

--- client/src/features/Event/types/Event.ts
+++ client/src/features/Event/types/Event.ts
@@ -12,6 +12,26 @@ export type Event = {
   organizerName: string;
 };
 
+export type EventDetail = {
+  eventId: number;
+  title: string;
+  description: string;
+  place: string;
+  organizerName: string;
+  eventStart: string;
+  eventEnd: string;
+  registrationStart: string;
+  registrationEnd: string;
+  currentGuestCount: number;
+  maxCapacity: number;
+  questions: {
+    questionId: number;
+    questionText: string;
+    isRequired: boolean;
+    orderIndex: number;
+  }[];
+};
+
 // S.TODO: 추후 Organization 폴더가 생기면 이동
 export type Organization = {
   organizationId: number;
