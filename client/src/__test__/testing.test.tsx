a4d10eacd24766db8f99493043743268ba794e76
keemsebeen <sebin0580@gmail.com>
2025-07-24 13:20:45 +0900

    [FE] 프론트엔드 ci/cd 환경 설정 (#167)

--- client/src/__test__/testing.test.tsx
+++ client/src/__test__/testing.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
-import { App } from '@/App';
+import { HomePage } from '@/features/Home/page/HomePage';
 
 describe('RTL Test', () => {
   it('should render', () => {
-    render(<App />);
-    expect(screen.getByText('React + TypeScript + Webpack')).toBeInTheDocument();
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    );
+    screen.debug();
+
+    expect(
+      screen.getByText('슬랙이나 메신저에서 참여하고 싶었던 이벤트를 놓친 경험 있으신가요?')
+    ).toBeInTheDocument();
   });
 });
