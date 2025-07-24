e058fbfc6516c9d3b7479988de93d6a433e8508a
keemsebeen <sebin0580@gmail.com>
2025-07-24 15:22:03 +0900

    chore: test ci 깨지는 에러 해결

--- client/src/__test__/testing.test.tsx
+++ client/src/__test__/testing.test.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
@@ -5,12 +6,16 @@ import { HomePage } from '@/features/Home/page/HomePage';
 
 describe('RTL Test', () => {
   it('should render', () => {
+    const queryClient = new QueryClient({
+      defaultOptions: { queries: { retry: false } },
+    });
     render(
       <MemoryRouter>
-        <HomePage />
+        <QueryClientProvider client={queryClient}>
+          <HomePage />
+        </QueryClientProvider>
       </MemoryRouter>
     );
-    screen.debug();
 
     expect(
       screen.getByText('슬랙이나 메신저에서 참여하고 싶었던 이벤트를 놓친 경험 있으신가요?')
