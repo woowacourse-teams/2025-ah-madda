e4dc5f970354f3d8a2254793d830719e2d1d4ec4
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-07-24 18:29:02 +0900

    fix: 주최자 참여자 api 잘못 동작하던 문제 수정

--- server/src/main/java/com/ahmadda/application/OrganizationMemberEventService.java
+++ server/src/main/java/com/ahmadda/application/OrganizationMemberEventService.java
@@ -1,5 +1,6 @@
 package com.ahmadda.application;
 
+import com.ahmadda.application.dto.LoginMember;
 import com.ahmadda.application.exception.NotFoundException;
 import com.ahmadda.domain.Event;
 import com.ahmadda.domain.EventRepository;
@@ -17,20 +18,20 @@ public class OrganizationMemberEventService {
     private final EventRepository eventRepository;
     private final OrganizationMemberRepository organizationMemberRepository;
 
-    public List<Event> getOwnerEvents(final Long organizationMemberId) {
-        OrganizationMember organizationMember = getOrganizationMember(organizationMemberId);
+    public List<Event> getOwnerEvents(final Long organizationId, final LoginMember loginMember) {
+        OrganizationMember organizationMember = getOrganizationMember(organizationId, loginMember);
 
         return eventRepository.findAllByOrganizer(organizationMember);
     }
 
-    public List<Event> getParticipantEvents(final Long organizationMemberId) {
-        OrganizationMember organizationMember = getOrganizationMember(organizationMemberId);
+    public List<Event> getParticipantEvents(final Long organizationId, final LoginMember loginMember) {
+        OrganizationMember organizationMember = getOrganizationMember(organizationId, loginMember);
 
         return organizationMember.getParticipatedEvents();
     }
 
-    private OrganizationMember getOrganizationMember(final Long organizationMemberId) {
-        return organizationMemberRepository.findById(organizationMemberId)
+    private OrganizationMember getOrganizationMember(final Long organizationId, final LoginMember loginMember) {
+        return organizationMemberRepository.findByOrganizationIdAndMemberId(organizationId, loginMember.memberId())
                 .orElseThrow(() -> new NotFoundException("존재하지 않은 조직원 정보입니다."));
     }
 }
