43a3b83cf35bfda859d7efa83f81fa70d34bc2e5
yeji0214 <1225yj50@naver.com>
2025-07-24 20:15:47 +0900

    refactor: testing.test.tsx 수정

--- client/src/__test__/testing.test.tsx
+++ client/src/__test__/testing.test.tsx
@@ -1,15 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { HomePage } from '@/features/Home/page/HomePage';
+import { HomePage } from '../features/Home/page/HomePage';
 
 describe('RTL Test', () => {
   it('should render', () => {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: { retry: false },
+        mutations: { retry: false },
+      },
+    });
+
     render(
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </QueryClientProvider>
     );
+
     screen.debug();
 
     expect(
