e9cc7cd63e0716c026682dc6d57fb1040089bb54
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-07-28 17:03:10 +0900

    [BE] 로그인 API 설명 스웨거 추가 (#229)

--- server/src/main/java/com/ahmadda/presentation/LoginController.java
+++ server/src/main/java/com/ahmadda/presentation/LoginController.java
@@ -3,6 +3,12 @@ package com.ahmadda.presentation;
 import com.ahmadda.application.LoginService;
 import com.ahmadda.presentation.dto.AccessTokenResponse;
 import com.ahmadda.presentation.dto.LoginRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +25,33 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    @Operation(summary = "구글 OAuth 로그인", description = "Google OAuth 인가 코드로 로그인을 할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    content = @Content(
+                            schema = @Schema(
+                                    implementation = AccessTokenResponse.class
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    value = """
+                                            {
+                                              "type": "about:blank",
+                                              "title": "Unauthorized",
+                                              "status": 401,
+                                              "detail": "유효하지 않은 인증 정보 입니다. 인가 코드가 만료되었거나, 잘못되었습니다.",
+                                              "instance": "/api/members/login"
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @PostMapping("/login")
     public ResponseEntity<AccessTokenResponse> login(@RequestBody final LoginRequest loginRequest) {
         String authTokens = loginService.login(loginRequest.code(), loginRequest.redirectUri());
