7db1b3ae8ea6a95a020598a7dafb44863aca7dce
장정안 <wjddks96@naver.com>
2025-07-28 20:18:49 +0900

    feat: ProtectRoute 구현

---
+++ client/src/router/ProtectRoute.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+import { Outlet, useNavigate } from 'react-router-dom';
+
+import { isAuthenticated } from '@/api/auth';
+
+export const ProtectRoute = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      alert('로그인이 필요한 서비스입니다. 먼저 로그인해 주세요.');
+      navigate('/');
+    }
+  }, [navigate]);
+
+  return <Outlet />;
+};
