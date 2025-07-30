ff73b7ea1d8c09d6c9e3fa211f3b8f7d6820051b
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-07-30 16:14:18 +0900

    refactor: prod 환경에서만 슬랙 알람이 나가도록 config 수정

--- server/src/main/java/com/ahmadda/infra/slack/SlackReminder.java
+++ server/src/main/java/com/ahmadda/infra/slack/SlackReminder.java
@@ -1,52 +1,8 @@
 package com.ahmadda.infra.slack;
 
-import com.ahmadda.domain.Member;
-import com.ahmadda.infra.slack.config.SlackAlarmProperties;
-import com.ahmadda.infra.slack.dto.MemberCreationAlarmPayload;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientException;
+import com.ahmadda.application.dto.MemberCreateAlarmDto;
 
-@Component
-@Slf4j
-@Profile("prod")
-@EnableConfigurationProperties(SlackAlarmProperties.class)
-public class SlackReminder {
+public interface SlackReminder {
 
-    private final SlackAlarmProperties slackAlarmProperties;
-    private final RestClient restClient;
-
-    public SlackReminder(final RestClient.Builder restClientBuilder, final SlackAlarmProperties slackAlarmProperties) {
-        restClientBuilder.requestFactory(simpleClientHttpRequestFactory());
-        restClientBuilder.baseUrl(slackAlarmProperties.getPostMessageUrl());
-        restClientBuilder.defaultHeaders(headers -> {
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setBearerAuth(slackAlarmProperties.getBotToken());
-        });
-
-        this.slackAlarmProperties = slackAlarmProperties;
-        this.restClient = restClientBuilder.build();
-    }
-
-    private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(slackAlarmProperties.getConnectTimeout());
-        factory.setReadTimeout(slackAlarmProperties.getReadTimeout());
-
-        return factory;
-    }
-
-    public void alarmMemberCreation(final Member member) {
-        try {
-            restClient.post()
-                    .body(MemberCreationAlarmPayload.create(member, slackAlarmProperties.getChannelId()));
-        } catch (RestClientException e) {
-            log.error("Slack 회원 가입 알림 전송에 실패했습니다. email={}, error={}", member.getEmail(), e.getMessage());
-        }
-    }
+    void alarmMemberCreation(final MemberCreateAlarmDto memberCreateAlarmDto);
 }
---
+++ server/src/main/java/com/ahmadda/infra/slack/config/SlackReminderConfig.java
@@ -0,0 +1,46 @@
+package com.ahmadda.infra.slack.config;
+
+import com.ahmadda.infra.slack.AsyncSlackReminder;
+import com.ahmadda.infra.slack.MockProductionSlackReminder;
+import com.ahmadda.infra.slack.SlackReminder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+@Profile("prod")
+public class SlackReminderConfig {
+
+    @Bean
+    @Profile("prod")
+    public SlackReminder slackReminder(final RestClient.Builder restClientBuilder,
+                                       final SlackAlarmProperties slackAlarmProperties) {
+
+        restClientBuilder.requestFactory(simpleClientHttpRequestFactory(slackAlarmProperties));
+        restClientBuilder.baseUrl(slackAlarmProperties.getPostMessageUrl());
+        restClientBuilder.defaultHeaders(headers -> {
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setBearerAuth(slackAlarmProperties.getBotToken());
+        });
+        RestClient restClient = restClientBuilder.build();
+
+        return new AsyncSlackReminder(restClient, slackAlarmProperties);
+    }
+
+    private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory(final SlackAlarmProperties slackAlarmProperties) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(slackAlarmProperties.getConnectTimeout());
+        factory.setReadTimeout(slackAlarmProperties.getReadTimeout());
+
+        return factory;
+    }
+
+    @Bean
+    @Profile("!prod")
+    public SlackReminder mockSlackReminder() {
+        return new MockProductionSlackReminder();
+    }
+}
