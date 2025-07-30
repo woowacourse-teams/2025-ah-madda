97ce7f93708396d7c7c328dd4e681e986c9e0a24
장정안 <wjddks96@naver.com>
2025-07-30 17:42:17 +0900

    feat: Tabs 컴포넌트를 EventDetailPage 내에서 사용

--- client/src/features/Event/Detail/pages/EventDetailPage.tsx
+++ client/src/features/Event/Detail/pages/EventDetailPage.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react';
 import { useSuspenseQueries } from '@tanstack/react-query';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -6,8 +7,10 @@ import { Flex } from '@/shared/components/Flex';
 import { Header } from '@/shared/components/Header';
 import { IconButton } from '@/shared/components/IconButton';
 import { PageLayout } from '@/shared/components/PageLayout';
+import { Tabs } from '@/shared/components/Tabs';
 import { Text } from '@/shared/components/Text';
 
+import { ApplicationsTab } from '../components/ApplicationsTab';
 import { EventDetailContent } from '../components/EventDetailContent';
 
 export const EventDetailPage = () => {
@@ -43,7 +46,24 @@ export const EventDetailPage = () => {
         />
       }
     >
-      <EventDetailContent isGuest={guestStatus.isGuest} {...event} />
+      <Tabs defaultValue="detail">
+        <Tabs.List
+          css={css`
+            margin-top: 59px;
+          `}
+        >
+          <Tabs.Trigger value="detail">이벤트 정보</Tabs.Trigger>
+          <Tabs.Trigger value="applications">신청 현황</Tabs.Trigger>
+        </Tabs.List>
+
+        <Tabs.Content value="detail">
+          <EventDetailContent isGuest={guestStatus.isGuest} {...event} />
+        </Tabs.Content>
+
+        <Tabs.Content value="applications">
+          <ApplicationsTab eventId={Number(eventId)} />
+        </Tabs.Content>
+      </Tabs>
     </PageLayout>
   );
 };
