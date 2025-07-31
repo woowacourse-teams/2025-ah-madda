519689b5082fd4ffcea6bd8c89f8a77ea1930312
keemsebeen <sebin0580@gmail.com>
2025-07-31 20:30:38 +0900

    feat: 아이콘에서 사용할 타입, colorMap 생성

--- client/src/shared/styles/colors.ts
+++ client/src/shared/styles/colors.ts
@@ -41,6 +41,18 @@ export const colors = {
   red700: '#E31E1A',
   red800: '#D61111',
   red900: '#C80000',
+
+  white: '#FFFFFF',
+  black: '#000000',
 };
 
 export type ColorType = typeof colors;
+export type IconColor = 'primary' | 'secondary' | 'gray' | 'red' | 'white';
+
+export const colorMap: Record<IconColor, string> = {
+  primary: colors.primary800,
+  secondary: colors.secondary900,
+  gray: colors.gray800,
+  red: colors.red700,
+  white: colors.white,
+};
