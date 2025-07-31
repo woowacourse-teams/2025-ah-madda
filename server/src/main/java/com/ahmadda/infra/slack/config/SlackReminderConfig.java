73a81d14ebfcae1d66c756a4465c26eea6164219
LeeJaeHoon <dlwogns3413@naver.com>
2025-07-31 13:55:03 +0900

    fix: Slack 프로퍼티 활성화 및 slack.yml import 추가

--- server/src/main/java/com/ahmadda/infra/slack/config/SlackReminderConfig.java
+++ server/src/main/java/com/ahmadda/infra/slack/config/SlackReminderConfig.java
@@ -3,6 +3,7 @@ package com.ahmadda.infra.slack.config;
 import com.ahmadda.infra.slack.AsyncSlackReminder;
 import com.ahmadda.infra.slack.MockProductionSlackReminder;
 import com.ahmadda.infra.slack.SlackReminder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,12 +12,15 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@EnableConfigurationProperties(SlackAlarmProperties.class)
 public class SlackReminderConfig {
 
     @Bean
     @Profile("prod")
-    public SlackReminder slackReminder(final RestClient.Builder restClientBuilder,
-                                       final SlackAlarmProperties slackAlarmProperties) {
+    public SlackReminder slackReminder(
+            final RestClient.Builder restClientBuilder,
+            final SlackAlarmProperties slackAlarmProperties
+    ) {
 
         restClientBuilder.requestFactory(simpleClientHttpRequestFactory(slackAlarmProperties));
         restClientBuilder.baseUrl(slackAlarmProperties.getPostMessageUrl());
