c2e0dabfd439f71fae28dd1a2957a4f155f10f4b
장정안 <wjddks96@naver.com>
2025-07-31 11:33:25 +0900

    feat: myEventPage 내 안내문구 추가

--- client/src/features/Event/My/pages/MyEventPage.tsx
+++ client/src/features/Event/My/pages/MyEventPage.tsx
@@ -1,4 +1,6 @@
+import { Flex } from '@/shared/components/Flex';
 import { PageLayout } from '@/shared/components/PageLayout';
+import { Text } from '@/shared/components/Text';
 
 import { EventTabs } from '../components/EventTabs';
 import { MyEventHeader } from '../components/MyEventHeader';
@@ -8,6 +10,11 @@ export const MyEventPage = () => {
   return (
     <PageLayout header={<MyEventHeader />}>
       <MyEventContainer>
+        <Flex justifyContent="center" margin="30px 0 30px 0">
+          <Text as="h2" type="Body" weight="medium" color="gray900">
+            내가 주최한 이벤트와 참여한 이벤트를 볼 수 있습니다.
+          </Text>
+        </Flex>
         <EventTabs />
       </MyEventContainer>
     </PageLayout>
