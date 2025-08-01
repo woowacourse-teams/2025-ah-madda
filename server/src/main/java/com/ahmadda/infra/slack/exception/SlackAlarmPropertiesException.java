ec625bf5235993e63b7b23eecaa7de370abcfea7
jeyong <joon6093@naver.com>
2025-08-01 10:26:11 +0900

    refactor: SlackAlarmProperties에서 검증하도록 변경

---
+++ server/src/main/java/com/ahmadda/infra/slack/exception/SlackAlarmPropertiesException.java
@@ -0,0 +1,8 @@
+package com.ahmadda.infra.slack.exception;
+
+public class SlackAlarmPropertiesException extends RuntimeException {
+
+    public SlackAlarmPropertiesException(String message) {
+        super(message);
+    }
+}
