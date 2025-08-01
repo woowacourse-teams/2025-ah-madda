f4b865c3c48c0b0cefbee4b457e13ce8433cf2d5
jeyong <joon6093@naver.com>
2025-08-01 10:27:46 +0900

    fix: redirectUri가 비어있는 문제 해결

--- server/src/main/java/com/ahmadda/infra/oauth/config/GoogleOAuthProperties.java
+++ server/src/main/java/com/ahmadda/infra/oauth/config/GoogleOAuthProperties.java
@@ -10,7 +10,6 @@ public class GoogleOAuthProperties {
 
     private final String clientId;
     private final String clientSecret;
-    private final String redirectUri;
     private final String tokenUri;
     private final String userUri;
     private final int connectTimeout;
@@ -19,17 +18,15 @@ public class GoogleOAuthProperties {
     public GoogleOAuthProperties(
             final String clientId,
             final String clientSecret,
-            final String redirectUri,
             final String tokenUri,
             final String userUri,
             final int connectTimeout,
             final int readTimeout
     ) {
-        validateProperties(clientId, clientSecret, redirectUri, tokenUri, userUri, connectTimeout, readTimeout);
+        validateProperties(clientId, clientSecret, tokenUri, userUri, connectTimeout, readTimeout);
 
         this.clientId = clientId;
         this.clientSecret = clientSecret;
-        this.redirectUri = redirectUri;
         this.tokenUri = tokenUri;
         this.userUri = userUri;
         this.connectTimeout = connectTimeout;
@@ -39,7 +36,6 @@ public class GoogleOAuthProperties {
     private void validateProperties(
             final String clientId,
             final String clientSecret,
-            final String redirectUri,
             final String tokenUri,
             final String userUri,
             final int connectTimeout,
@@ -50,9 +46,6 @@ public class GoogleOAuthProperties {
         }
         if (clientSecret == null || clientSecret.isBlank()) {
             throw new GoogleOAuthPropertiesException("Google OAuth clientSecret이 비어있습니다.");
-        }
-        if (redirectUri == null || redirectUri.isBlank()) {
-            throw new GoogleOAuthPropertiesException("Google OAuth redirectUri가 비어있습니다.");
         }
         if (tokenUri == null || tokenUri.isBlank()) {
             throw new GoogleOAuthPropertiesException("Google OAuth tokenUri가 비어있습니다.");
