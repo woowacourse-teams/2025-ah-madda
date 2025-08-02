a39345b8426bd37b29cf7450c6ff5275ad92b226
keemsebeen <sebin0580@gmail.com>
2025-08-02 21:36:49 +0900

    feat: 신청 마감일, 신청 여부에 따른 분기처리 추가

--- client/src/features/Event/Detail/components/SubmitButtonCard.tsx
+++ client/src/features/Event/Detail/components/SubmitButtonCard.tsx
@@ -1,3 +1,6 @@
+import { HTTPError } from 'ky';
+
+import { useCancelParticipate } from '@/api/mutations/useCancelParticipate';
 import { useParticipateEvent } from '@/api/mutations/useParticipateEvent';
 import { Answer, GuestStatusAPIResponse } from '@/api/types/event';
 import { Button } from '@/shared/components/Button';
@@ -18,10 +21,11 @@ export const SubmitButtonCard = ({
   const now = new Date();
   const isBeforeDeadline = now <= new Date(registrationEnd);
 
-  const { mutate } = useParticipateEvent(eventId);
+  const { mutate: participantMutate } = useParticipateEvent(eventId);
+  const { mutate: cancelParticipateMutate } = useCancelParticipate(eventId);
 
-  const handleClick = () => {
-    mutate(answers, {
+  const handleParticipantClick = () => {
+    participantMutate(answers, {
       onSuccess: () => {
         alert('✅ 참가 신청이 완료되었습니다.');
       },
@@ -31,15 +35,36 @@ export const SubmitButtonCard = ({
     });
   };
 
+  const handelCancelParticipateClick = () => {
+    cancelParticipateMutate(undefined, {
+      onSuccess: () => {
+        alert('✅ 참가 신청이 취소되었습니다.');
+      },
+      onError: (error) => {
+        if (error instanceof HTTPError) {
+          error.response.json().then((errorData) => {
+            alert(`❌ ${errorData.detail}`);
+          });
+        }
+      },
+    });
+  };
+
   return (
     <Flex margin="10px 0 40px">
       <Button
         width="100%"
         color={!isGuest || isBeforeDeadline ? '#2563EB' : 'gray'}
-        disabled={!isBeforeDeadline || isGuest}
-        onClick={handleClick}
+        disabled={!isBeforeDeadline}
+        onClick={isGuest ? handelCancelParticipateClick : handleParticipantClick}
       >
-        {isBeforeDeadline ? (!isGuest ? '신청 하기' : '신청 완료') : '신청 마감'}
+        {isBeforeDeadline
+          ? isGuest
+            ? '신청 취소'
+            : '신청 하기'
+          : isGuest
+            ? '신청 완료'
+            : '신청 마감'}
       </Button>
     </Flex>
   );
