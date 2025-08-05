5fa0530a3a218495eb5b211d1c89f4549f304cac
jeyong <joon6093@naver.com>
2025-08-05 21:51:32 +0900

    fix: 메일/푸시 알림 로직에서 영속성 컨텍스트 관련 문제 해결

--- server/src/main/java/com/ahmadda/infra/notification/push/FcmPushNotifier.java
+++ server/src/main/java/com/ahmadda/infra/notification/push/FcmPushNotifier.java
@@ -46,7 +46,7 @@ public class FcmPushNotifier implements PushNotifier {
         }
     }
 
-    private List<String> getRegistrationTokens(List<OrganizationMember> recipients) {
+    private List<String> getRegistrationTokens(final List<OrganizationMember> recipients) {
         List<Long> memberIds = recipients.stream()
                 .map(organizationMember -> organizationMember.getMember()
                         .getId())
