dc3af6a2a86375a7a46153a354a557a40f78c9e7
장정안 <wjddks96@naver.com>
2025-08-05 13:25:33 +0900

    feat: route에 잘못된 경로 접근 시 에러페이지로 이동 추가

--- client/src/router/route.tsx
+++ client/src/router/route.tsx
@@ -56,6 +56,10 @@ export const router = createBrowserRouter(
             },
           ],
         },
+        {
+          path: '*',
+          Component: ErrorPage,
+        },
       ],
     },
   ],
