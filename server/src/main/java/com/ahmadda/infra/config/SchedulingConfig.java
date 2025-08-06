7c64248a89ff97e78e07be9efb1d8362b75c03c6
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-08-06 17:41:33 +0900

    [BE] 등록 마감 임박 이벤트의 비게스트에게 알람 기능 구현 by 머피 (#383)
    Co-authored-by: jeyong <joon6093@naver.com>

---
+++ server/src/main/java/com/ahmadda/infra/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package com.ahmadda.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// TODO. 추후 ThreadPoolTaskScheduler 설정 추가 필요
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+}
