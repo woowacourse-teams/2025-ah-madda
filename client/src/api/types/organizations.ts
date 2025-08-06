ffa55fa4e8766d7b4e41a522090394b36b3db35e
keemsebeen <sebin0580@gmail.com>
2025-08-06 15:35:34 +0900

    feat: OrganizationParticipateAPIRequest 타입 추가

--- client/src/api/types/organizations.ts
+++ client/src/api/types/organizations.ts
@@ -2,3 +2,8 @@ export type InviteCodeAPIResponse = {
   inviteCode: string;
   expiresAt: string;
 };
+
+export type OrganizationParticipateAPIRequest = {
+  nickname: string;
+  inviteCode: string;
+};
