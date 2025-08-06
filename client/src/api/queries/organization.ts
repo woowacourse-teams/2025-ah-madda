1a80d3675429c9bc88d0abea4a6494010481a39b
keemsebeen <sebin0580@gmail.com>
2025-08-06 10:33:16 +0900

    feat: 특정 이벤트의 주최자 조회 api 함수 구현

--- client/src/api/queries/organization.ts
+++ client/src/api/queries/organization.ts
@@ -3,6 +3,7 @@ import { queryOptions } from '@tanstack/react-query';
 import { Event, Organization } from '@/features/Event/types/Event';
 
 import { fetcher } from '../fetcher';
+import { OrganizerStatusAPIResponse } from '../types/event';
 
 export const organizationQueryKeys = {
   all: () => ['organization'],
@@ -32,6 +33,12 @@ export const organizationQueryOptions = {
 
 const getAllEventAPI = ({ organizationId }: { organizationId: number }) => {
   return fetcher.get<Event[]>(`organizations/${organizationId}/events`);
+};
+
+const getOrganizerStatus = async (eventId: number) => {
+  return await fetcher.get<OrganizerStatusAPIResponse>(
+    `organizations/events/${eventId}/organizer-status`
+  );
 };
 
 const getOrganization = ({ organizationId }: { organizationId: string }) => {
