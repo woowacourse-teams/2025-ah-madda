34b2206536e91bd70b368b1a75adca42dcff0fd4
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2025-08-06 12:43:52 +0900

    [FE] 센트리 설정 & 슬랙 연동  (#337)
    Co-authored-by: keemsebeen <sebin0580@gmail.com>

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,8 +6,12 @@ import { RouterProvider } from 'react-router-dom';
 
 import './reset.css';
 import { ClientQueryProvider } from './api/ClientQueryProvider';
+import { initSentry } from './lib/sentry';
 import { router } from './router/route';
 import { theme } from './shared/styles/theme';
+
+
+initSentry();
 
 const GA_ID = process.env.GOOGLE_ANALYTICS_ID;
 
@@ -26,6 +30,7 @@ if (GA_ID) {
   window.gtag('js', new Date());
   window.gtag('config', GA_ID);
 }
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
