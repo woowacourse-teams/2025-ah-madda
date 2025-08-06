ca4b09130fe2634d5ef75e7bb8b29c436f8e86ef
keemsebeen <sebin0580@gmail.com>
2025-08-06 15:43:15 +0900

    feat: useParticipateOrganization hook 구현

---
+++ client/src/api/mutations/useParticipateOrganization.ts
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { fetcher } from '../fetcher';
+import { OrganizationParticipateAPIRequest } from '../types/organizations';
+
+const participantOrganization = async (
+  organizationId: number,
+  data: OrganizationParticipateAPIRequest
+) => {
+  return fetcher.post<OrganizationParticipateAPIRequest>(
+    `organizations/${organizationId}/participation`,
+    data
+  );
+};
+
+export const useParticipateOrganization = (organizationId: number) => {
+  return useMutation({
+    mutationFn: (data: OrganizationParticipateAPIRequest) =>
+      participantOrganization(organizationId, data),
+  });
+};
