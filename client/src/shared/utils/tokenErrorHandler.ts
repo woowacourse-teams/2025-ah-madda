7ee12150eecbd330006b2a1a4e58d20b3e663f66
장정안 <wjddks96@naver.com>
2025-08-06 22:34:34 +0900

    refactor: 서버에서 내려주는 에러 메세지와 똑같게 수정

--- client/src/shared/utils/tokenErrorHandler.ts
+++ client/src/shared/utils/tokenErrorHandler.ts
@@ -3,7 +3,7 @@ import { ACCESS_TOKEN_KEY } from '@/shared/constants';
 import { removeLocalStorage } from '@/shared/utils/localStorage';
 
 export const tokenErrorHandler = (responseText: HttpErrorResponse) => {
-  if (responseText.detail === '인증 토큰 정보가 존재하지 않거나 유효하지 않습니다.') {
+  if (responseText.detail === '만료기한이 지난 토큰입니다.') {
     removeLocalStorage(ACCESS_TOKEN_KEY);
     alert('로그인이 만료되었습니다. 다시 로그인해 주세요.');
     window.location.href = '/';
