4289c3c7905bedf8a0dc14b20be3dc9a5329519d
keemsebeen <sebin0580@gmail.com>
2025-08-06 12:02:31 +0900

    [FE] 관리페이지 체크박스 기반 이메일 발송 (#368)

--- client/src/features/Event/Detail/components/GuestItem.tsx
+++ client/src/features/Event/Detail/components/GuestItem.tsx
@@ -1,31 +1,23 @@
 import styled from '@emotion/styled';
 
-import { CheckBox } from '@/shared/components/CheckBox';
 import { Flex } from '@/shared/components/Flex';
 import { Text } from '@/shared/components/Text';
 
-import { GUEST_STYLES } from '../constants';
-import { Guest, NonGuest } from '../types';
+import { GUEST_STYLES } from '../../Manage/constants';
+import { Guest, NonGuest } from '../../Manage/types';
 
 type GuestItemProps = {
-  onGuestChecked: (organizationMemberId: number) => void;
   guest: Guest | NonGuest;
 };
 
 type GuestItemVariant = 'completed' | 'pending';
 
-export const GuestItem = ({ guest, onGuestChecked }: GuestItemProps) => {
+export const GuestItem = ({ guest }: GuestItemProps) => {
   const isGuest = 'guestId' in guest;
   const variant = isGuest ? 'completed' : 'pending';
 
   return (
-    <Flex width="100%" gap="8px" alignItems="center" padding="0 0 0 20px">
-      <Flex width="30px">
-        <CheckBox
-          checked={guest.isChecked}
-          onClick={() => onGuestChecked(guest.organizationMemberId)}
-        />
-      </Flex>
+    <Flex width="100%" gap="8px" alignItems="center">
       <StyledGuestItemContainer variant={variant} alignItems="center" padding="12px">
         <Text type="Label" weight="regular" color={GUEST_STYLES.common.nameTextColor}>
           {guest.nickname}
@@ -41,18 +33,15 @@ type GuestItemContainerProps = {
 
 const getContainerStyles = (variant: GuestItemVariant) => {
   if (variant === 'completed') {
-    //E.TODO: 추후 클릭 시 모달 로직이 추가되면, 호버 효과(색상) 추가
     return `
       background-color: #F0FDF4;
     `;
   }
-
   return `
     background-color: #F9FAFB;
   `;
 };
 
-//E.TODO: 추후 클릭 시 모달 로직이 추가되면, cursor: pointer; 추가
 export const StyledGuestItemContainer = styled(Flex)<GuestItemContainerProps>`
   width: 100%;
   border-radius: 8px;
