741ee70ee084e7928091c1efd8a640603d1fbfa6
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-08-06 17:42:28 +0900

    refactor: 명세서 array를 반환하도록 수정

--- server/src/main/java/com/ahmadda/presentation/EventStatisticController.java
+++ server/src/main/java/com/ahmadda/presentation/EventStatisticController.java
@@ -1,20 +1,12 @@
 package com.ahmadda.presentation;
 
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ahmadda.application.EventStatisticService;
 import com.ahmadda.application.dto.LoginMember;
 import com.ahmadda.domain.EventViewMetric;
 import com.ahmadda.presentation.dto.EventViewMetricResponse;
 import com.ahmadda.presentation.resolver.AuthMember;
-
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -22,6 +14,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "이벤트 통계", description = "이벤트 통계 관련 API")
 @RestController
@@ -35,7 +34,7 @@ public class EventStatisticController {
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = EventViewMetricResponse.class))
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = EventViewMetricResponse.class)))
             ),
             @ApiResponse(
                     responseCode = "401",
