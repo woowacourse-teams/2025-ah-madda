44f548b29b62b2d691bf2b3140f4b3fe21283381
keemsebeen <sebin0580@gmail.com>
2025-08-07 01:18:07 +0900

    feat: Icon asset 추가

--- client/src/shared/components/Icon/assets/index.ts
+++ client/src/shared/components/Icon/assets/index.ts
@@ -1,6 +1,7 @@
 import { Alarm } from './alarm';
 import { Back } from './back';
 import { Calendar } from './calendar';
+import { Chart } from './chart';
 import { Clock } from './clock';
 import { Close } from './close';
 import { Delete } from './delete';
@@ -12,6 +13,7 @@ import { User } from './user';
 export const Icons = {
   back: Back,
   calendar: Calendar,
+  chart: Chart,
   clock: Clock,
   close: Close,
   delete: Delete,
