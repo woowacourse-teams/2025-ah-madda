ac3599b6f6c366ee6ae01bc1f05a92dadef6119b
장정안 <wjddks96@naver.com>
2025-08-07 14:41:00 +0900

    refactor: 반복되는 로직 반복문으로 리팩토링

--- client/src/api/mutations/useCloseEventRegistration.ts
+++ client/src/api/mutations/useCloseEventRegistration.ts
@@ -13,17 +13,17 @@ export const useCloseEventRegistration = () => {
   return useMutation({
     mutationFn: (eventId: number) => closeEventRegistration(eventId),
     onSuccess: (eventId) => {
-      queryClient.invalidateQueries({
-        queryKey: [...eventQueryKeys.detail(), eventId],
-      });
-      queryClient.invalidateQueries({
-        queryKey: [...eventQueryKeys.guests(), eventId],
-      });
-      queryClient.invalidateQueries({
-        queryKey: [...eventQueryKeys.nonGuests(), eventId],
-      });
-      queryClient.invalidateQueries({
-        queryKey: [...eventQueryKeys.guestStatus(), eventId],
+      const keys = [
+        [...eventQueryKeys.detail(), eventId],
+        [...eventQueryKeys.guests(), eventId],
+        [...eventQueryKeys.nonGuests(), eventId],
+        [...eventQueryKeys.guestStatus(), eventId],
+      ];
+
+      keys.forEach((key) => {
+        queryClient.invalidateQueries({
+          queryKey: key,
+        });
       });
     },
   });
