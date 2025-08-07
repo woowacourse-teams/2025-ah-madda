42b2f6b1b906e023abea826d5ad92514ee3a5d3e
yeji0214 <1225yj50@naver.com>
2025-08-07 16:40:19 +0900

    refactor: 불필요한 코드 제거

--- client/src/api/mutations/useUpdateEvent.ts
+++ client/src/api/mutations/useUpdateEvent.ts
@@ -1,15 +1,10 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { fetcher } from '../fetcher';
 import { UpdateEventParams, UpdateEventResponse } from '../types/event';
 
 export const useUpdateEvent = () => {
-  const queryClient = useQueryClient();
-
   return useMutation<UpdateEventResponse, Error, UpdateEventParams>({
     mutationFn: ({ eventId, payload }) => fetcher.patch(`organizations/events/${eventId}`, payload),
-    onSuccess: () => {
-      queryClient.invalidateQueries();
-    },
   });
 };
