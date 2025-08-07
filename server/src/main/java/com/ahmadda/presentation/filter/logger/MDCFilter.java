24ad252f31d2a8b47591af6b946abb0a2276148c
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-08-07 12:50:48 +0900

    feat: 로깅 프레임워크 적용

---
+++ server/src/main/java/com/ahmadda/presentation/filter/logger/MDCFilter.java
@@ -0,0 +1,39 @@
+package com.ahmadda.presentation.filter.logger;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class MDCFilter implements Filter {
+
+    private final String REQUEST_ID = "request_id";
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws IOException, ServletException {
+        try {
+            MDC.put(REQUEST_ID, generateRequestId());
+            chain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+
+    private String generateRequestId() {
+        return UUID.randomUUID()
+                .toString()
+                .substring(0, 8);
+    }
+}
