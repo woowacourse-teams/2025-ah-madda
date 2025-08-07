7df7e53cc3fe6028a4d7a3eb6ddc6046108a13d1
yeji0214 <1225yj50@naver.com>
2025-08-07 15:14:16 +0900

    refactor: FIELD_CONFIG의 required 필수 속성으로 변경

--- client/src/features/Event/New/constants/formFieldConfig.ts
+++ client/src/features/Event/New/constants/formFieldConfig.ts
@@ -1,15 +1,15 @@
 import { BasicEventFormFields } from '../../types/Event';
 
 type FieldConfig = {
-  required?: boolean;
+  required: boolean;
   type: 'string' | 'number';
 };
 
 export const FIELD_CONFIG: Record<keyof BasicEventFormFields, FieldConfig> = {
   title: { required: true, type: 'string' },
-  description: { type: 'string' },
-  place: { type: 'string' },
-  maxCapacity: { type: 'number' },
+  description: { required: false, type: 'string' },
+  place: { required: false, type: 'string' },
+  maxCapacity: { required: false, type: 'number' },
   eventStart: { required: true, type: 'string' },
   eventEnd: { required: true, type: 'string' },
   registrationEnd: { required: true, type: 'string' },
