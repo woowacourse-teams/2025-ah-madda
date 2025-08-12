172d21da5d9d3e7c3c84d0f44a8db640bb6a197d
jumdo12 <jumdo12@gmail.com>
2025-08-12 15:47:32 +0900

    feat: BaseEntity 추가

--- server/src/main/java/com/ahmadda/domain/EventTemplate.java
+++ server/src/main/java/com/ahmadda/domain/EventTemplate.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class EventTemplate {
+public class EventTemplate extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
