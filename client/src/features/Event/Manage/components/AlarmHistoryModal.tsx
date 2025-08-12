4d81c1a73350c538bfa8f1b2d665cb2eb3312709
keemsebeen <sebin0580@gmail.com>
2025-08-12 22:04:59 +0900

    feat: AlarmHistoryModal 컴포넌트 구현

---
+++ client/src/features/Event/Manage/components/AlarmHistoryModal.tsx
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { NotifyHistoryAPIResponse } from '@/api/types/event';
+import { Badge } from '@/shared/components/Badge';
+import { Flex } from '@/shared/components/Flex';
+import { Modal } from '@/shared/components/Modal';
+import { ModalProps } from '@/shared/components/Modal/Modal';
+import { Text } from '@/shared/components/Text';
+import { theme } from '@/shared/styles/theme';
+
+import { HistoryContainer } from '../containers/HistoryContainer';
+
+import { HistoryCard } from './HistoryCard';
+
+type AlarmHistoryModalProps = { notifyData: NotifyHistoryAPIResponse[] } & ModalProps;
+
+export const AlarmHistoryModal = ({ notifyData, isOpen, onClose }: AlarmHistoryModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      css={css`
+        width: 380px;
+      `}
+    >
+      <Text as="h3" type="Heading" weight="bold">
+        알림 내역
+      </Text>
+      <HistoryContainer>
+        {notifyData.map((history, index) => (
+          <HistoryCard key={index} {...history} />
+        ))}
+      </HistoryContainer>
+    </Modal>
+  );
+};
