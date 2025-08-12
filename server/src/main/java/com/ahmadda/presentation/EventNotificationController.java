208cb5f28d0fac2829e9dff9d3852dd9e2aa9caf
jeyong <joon6093@naver.com>
2025-08-12 14:23:43 +0900

    refactor: 사용하지 않는 미참여 알람 기능 제거

--- server/src/main/java/com/ahmadda/presentation/EventNotificationController.java
+++ server/src/main/java/com/ahmadda/presentation/EventNotificationController.java
@@ -2,7 +2,6 @@ package com.ahmadda.presentation;
 
 import com.ahmadda.application.EventNotificationService;
 import com.ahmadda.application.dto.LoginMember;
-import com.ahmadda.application.dto.NonGuestsNotificationRequest;
 import com.ahmadda.application.dto.SelectedOrganizationMembersNotificationRequest;
 import com.ahmadda.presentation.resolver.AuthMember;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,72 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventNotificationController {
 
     private final EventNotificationService eventNotificationService;
-
-    @Operation(summary = "이벤트 미참여 조직원에게 알림 발송", description = "주최자가 이벤트에 참여하지 않은 조직원들에게 이메일 알림을 발송합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(
-                    responseCode = "401",
-                    content = @Content(
-                            examples = @ExampleObject(
-                                    value = """
-                                            {
-                                              "type": "about:blank",
-                                              "title": "Unauthorized",
-                                              "status": 401,
-                                              "detail": "유효하지 않은 인증 정보 입니다.",
-                                              "instance": "/api/events/{eventId}/notify-non-guests"
-                                            }
-                                            """
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "403",
-                    content = @Content(
-                            examples = @ExampleObject(
-                                    value = """
-                                            {
-                                              "type": "about:blank",
-                                              "title": "Forbidden",
-                                              "status": 403,
-                                              "detail": "이벤트 주최자가 아닙니다.",
-                                              "instance": "/api/events/{eventId}/notify-non-guests"
-                                            }
-                                            """
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    content = @Content(
-                            examples = @ExampleObject(
-                                    value = """
-                                            {
-                                              "type": "about:blank",
-                                              "title": "Not Found",
-                                              "status": 404,
-                                              "detail": "존재하지 않는 이벤트입니다.",
-                                              "instance": "/api/events/{eventId}/notify-non-guests"
-                                            }
-                                            """
-                            )
-                    )
-            )
-    })
-    @Deprecated
-    @PostMapping("/{eventId}/notify-non-guests")
-    public ResponseEntity<Void> notifyNonGuests(
-            @PathVariable final Long eventId,
-            @RequestBody @Valid final NonGuestsNotificationRequest request,
-            @AuthMember final LoginMember loginMember
-    ) {
-        eventNotificationService.notifyNonGuestOrganizationMembers(eventId, request, loginMember);
-
-        return ResponseEntity.ok()
-                .build();
-    }
-
+    
     @Operation(
             summary = "선택된 조직원에게 알림 발송",
             description = "주최자가 선택한 조직원들에게 이메일 알림을 발송합니다."
