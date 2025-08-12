e4be90253579c2c5a0fdf67bd854f799bb962922
keemsebeen <sebin0580@gmail.com>
2025-08-13 01:20:23 +0900

    [FE] 이벤트 전체 조회 페이지 디자인 변경 (#466)

--- client/src/shared/components/Card/Card.styled.ts
+++ client/src/shared/components/Card/Card.styled.ts
@@ -2,9 +2,9 @@ import styled from '@emotion/styled';
 
 export const StyledCard = styled.div`
   width: 100%;
-  padding: 1.375rem;
+  padding: 30px;
   box-sizing: border-box;
-  border-radius: 0.875rem;
+  border-radius: 12px;
   border: 1px solid #e5e7eb;
   background-color: #ffffff;
 `;
