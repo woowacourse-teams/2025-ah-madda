554fe01e537b2fb99857d6df0a90e4a357d1e8aa
keemsebeen <sebin0580@gmail.com>
2025-08-12 14:55:37 +0900

    chore: 불필요한 코드 제거

--- client/src/features/Event/Overview/components/EventCard.tsx
+++ client/src/features/Event/Overview/components/EventCard.tsx
@@ -84,7 +84,6 @@ export const EventCard = ({
           </Text>
         </Flex>
         <Spacing height="2px" />
-        {/* {formatTime(registrationEnd)} */}
         <Flex justifyContent="space-between" alignItems="center" gap="20px">
           <ProgressBar value={progressValue} max={progressMax} color={theme.colors.primary500} />
           <Text type="Label" color="#99A1AF" weight="semibold">
