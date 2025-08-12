0152403eb9c44440d6361195aa43b778fcd09011
keemsebeen <sebin0580@gmail.com>
2025-08-12 22:04:32 +0900

    feat: HistoryCard 컴포넌트 구현

---
+++ client/src/features/Event/Manage/components/HistoryCard.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+import { NotifyHistoryAPIResponse } from '@/api/types/event';
+import { Badge } from '@/shared/components/Badge';
+import { Flex } from '@/shared/components/Flex';
+import { Text } from '@/shared/components/Text';
+import { theme } from '@/shared/styles/theme';
+
+import { formatKoreanDateTime } from '../../Detail/utils/formatKoreanDateTime';
+
+export const HistoryCard = ({ recipientCount, content, sentAt }: NotifyHistoryAPIResponse) => {
+  return (
+    <StyledHistoryCard
+      dir="column"
+      justifyContent="space-between"
+      alignItems="flex-start"
+      gap="4px"
+    >
+      <Flex width="100%" justifyContent="space-between" alignItems="center">
+        <Text type="Body" weight="semibold">
+          {content}
+        </Text>
+        <Badge variant="blue">{recipientCount}명</Badge>
+      </Flex>
+      <Text type="Label" color={theme.colors.gray500}>
+        {formatKoreanDateTime(sentAt)}
+      </Text>
+    </StyledHistoryCard>
+  );
+};
+
+const StyledHistoryCard = styled(Flex)`
+  padding: 12px;
+  border-bottom: 1px solid ${theme.colors.gray200};
+`;
