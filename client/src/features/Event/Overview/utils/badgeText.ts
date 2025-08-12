00fa10e7acbc158f3eba9fc0efb34d7adc97cc67
keemsebeen <sebin0580@gmail.com>
2025-08-12 15:57:02 +0900

    refactor: badgeText 구조 수정

--- client/src/features/Event/Overview/utils/badgeText.ts
+++ client/src/features/Event/Overview/utils/badgeText.ts
@@ -1,18 +1,7 @@
 export const badgeText = (registrationEnd: string) => {
-  if (new Date(registrationEnd) < new Date()) {
-    return {
-      color: 'red',
-      text: '신청마감',
-    };
-  }
   if (new Date() < new Date(registrationEnd)) {
-    return {
-      color: 'blue',
-      text: '모집중',
-    };
+    return { color: 'blue', text: '모집중' };
   }
-  return {
-    color: 'gray',
-    text: '예정',
-  };
+
+  return { color: 'red', text: '신청마감' };
 };
