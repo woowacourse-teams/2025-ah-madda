bca84a82462f9ab34946fe84d4658bd5aa047c57
keemsebeen <sebin0580@gmail.com>
2025-08-13 21:45:44 +0900

    fix: Header 스토리북 에러 해결

--- client/src/shared/components/Header/Header.stories.tsx
+++ client/src/shared/components/Header/Header.stories.tsx
@@ -1,32 +1,40 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { Button } from '../Button';
+import { Flex } from '../Flex';
+import { Icon } from '../Icon';
+
 import { Header } from './Header';
 
 const meta = {
   title: 'components/Header',
   component: Header,
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The Header component is a customizable header that can contain a left and right element.',
+      },
+    },
+  },
 } satisfies Meta<typeof Header>;
 
 export default meta;
 type Story = StoryObj<typeof Header>;
 
-export const LeftOnly: Story = {
-  args: {
-    left: <h2 style={{ margin: 0 }}>아맞다!</h2>,
-  },
-  render: (args) => <Header {...args} />,
+export const Basic: Story = {
+  render: () => (
+    <Flex justifyContent="center" alignItems="center" height="30vh">
+      <Header left={<Icon name="logo" />} />
+    </Flex>
+  ),
 };
 
 export const LeftAndRight: Story = {
   args: {
-    left: <h2 style={{ margin: 0 }}>아맞다!</h2>,
-    right: (
-      <div style={{ display: 'flex', gap: '12px' }}>
-        <button>로그아웃</button>
-        <button>내 이벤트</button>
-      </div>
-    ),
+    left: <Icon name="logo" />,
+    right: <Button size="sm">로그아웃</Button>,
   },
   render: (args) => <Header {...args} />,
 };
