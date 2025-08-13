c2e1e454edd7a74e026bf57eb43ff0ffc4dd5b5d
YEJI <91647696+yeji0214@users.noreply.github.com>
2025-08-13 10:45:40 +0900

    [FE] 이벤트 상세 정보 및 생성 페이지 테스트 작성 (#185)

--- client/src/__test__/testing.test.tsx
+++ client/src/__test__/testing.test.tsx
@@ -2,19 +2,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { HomePage } from '@/features/Home/page/HomePage';
+import { HomePage } from '../features/Home/page/HomePage';
 
 describe('RTL Test', () => {
   it('should render', () => {
     const queryClient = new QueryClient({
-      defaultOptions: { queries: { retry: false } },
+      defaultOptions: {
+        queries: { retry: false },
+        mutations: { retry: false },
+      },
     });
+
     render(
-      <MemoryRouter>
-        <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
           <HomePage />
-        </QueryClientProvider>
-      </MemoryRouter>
+        </MemoryRouter>
+      </QueryClientProvider>
     );
 
     expect(screen.getByText('채널이 너무 많아 정보가 분산되어 알 수 없었다.')).toBeInTheDocument();
