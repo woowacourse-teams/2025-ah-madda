b64963c98e76b1955a7c6fba00f3496cbbce6ee6
keemsebeen <sebin0580@gmail.com>
2025-08-14 13:28:53 +0900

    feat: footer 컴포넌트 구현

---
+++ client/src/shared/components/Footer/Footer.tsx
@@ -0,0 +1,51 @@
+import { css } from '@emotion/react';
+
+import { theme } from '@/shared/styles/theme';
+
+import Ahmadda from '../../../../public/favicon-light.png';
+import Github from '../../../../public/github.png';
+import { Flex } from '../Flex';
+import { Icon } from '../Icon';
+import { Spacing } from '../Spacing';
+import { Text } from '../Text';
+
+import { StyledFooterContainer, StyledLinkButton } from './Footer.styled';
+
+export const Footer = () => {
+  return (
+    <StyledFooterContainer>
+      <Flex
+        justifyContent="center"
+        margin="0 auto"
+        padding="20px"
+        css={css`
+          max-width: 1120px;
+        `}
+      >
+        <Flex dir="column" width="100%" gap="10px">
+          <Flex alignItems="center">
+            <img src={Ahmadda} alt="logo" width="30" height="30" />
+            <Icon name="logo" />
+          </Flex>
+          <Text type="Body" weight="semibold" color={theme.colors.gray600}>
+            우아한테크코스 - 아맞다팀
+          </Text>
+          <Spacing height="10px" />
+          <Text type="Label" color={theme.colors.gray600}>
+            {`Copyright ⓒ ahmadda.\nAll Rights Reserved E-mail: amadda.team@gmail.com`}
+          </Text>
+        </Flex>
+        <Flex justifyContent="center" alignItems="center" gap="8px" height="40px">
+          <StyledLinkButton href="https://pleasant-goat-041.notion.site/24ff55e0580c80c09bcfc5ce5da493f5?source=copy_link">
+            <Text type="Body" weight="semibold" color={theme.colors.gray600}>
+              팀 소개
+            </Text>
+          </StyledLinkButton>
+          <StyledLinkButton href="https://github.com/woowacourse-teams/2025-ah-madda">
+            <img src={Github} alt="github" />
+          </StyledLinkButton>
+        </Flex>
+      </Flex>
+    </StyledFooterContainer>
+  );
+};
