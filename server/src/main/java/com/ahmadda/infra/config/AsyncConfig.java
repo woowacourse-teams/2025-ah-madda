ee14b4460dc7d859d4b0535a382ba49e3091dce5
jeyong <joon6093@naver.com>
2025-08-14 22:46:18 +0900

    feat: MDC 컨텍스트가 전파되도록 TaskDecorator 적용

--- server/src/main/java/com/ahmadda/infra/config/AsyncConfig.java
+++ server/src/main/java/com/ahmadda/infra/config/AsyncConfig.java
@@ -1,11 +1,46 @@
 package com.ahmadda.infra.config;
 
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-// TODO. 추후 ThreadPoolTaskExecutor 설정 추가 필요
+import java.util.Map;
+import java.util.concurrent.Executor;
+
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
+    // TODO. 추후 ThreadPoolTaskExecutor 설정 변경 필요
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("async-");
+        executor.setTaskDecorator(new AsyncMDCTaskDecorator());
+        executor.initialize();
+
+        return executor;
+    }
+
+    private static class AsyncMDCTaskDecorator implements TaskDecorator {
+
+        @Override
+        public Runnable decorate(final Runnable runnable) {
+            Map<String, String> contextMap = MDC.getCopyOfContextMap();
+
+            return () -> {
+                try {
+                    if (contextMap != null) {
+                        MDC.setContextMap(contextMap);
+                    }
+                    runnable.run();
+                } finally {
+                    MDC.clear();
+                }
+            };
+        }
+    }
 }
