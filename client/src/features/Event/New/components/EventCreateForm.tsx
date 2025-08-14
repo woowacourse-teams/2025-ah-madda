ecae39b99fca32880a2ea10f9a0d9a4dd4252b7a
yeji0214 <1225yj50@naver.com>
2025-08-14 13:24:36 +0900

    fix: 불필요한 css 제거

--- client/src/features/Event/New/components/EventCreateForm.tsx
+++ client/src/features/Event/New/components/EventCreateForm.tsx
@@ -307,7 +307,7 @@ export const EventCreateForm = ({ isEdit, eventId }: EventCreateFormProps) => {
                 cursor: pointer;
 
                 &:hover {
-                  background: transparent !important;
+                  background: transparent;
                 }
               `}
             >
