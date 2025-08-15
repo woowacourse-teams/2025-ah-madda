3d5200b13236a741f50fc2e514201d08cdfc880e
장정안 <wjddks96@naver.com>
2025-08-15 20:20:40 +0900

    design: object-fit을 cover로 변경

--- client/src/shared/components/Avatar/Avatar.styled.ts
+++ client/src/shared/components/Avatar/Avatar.styled.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 export const StyledAvatarImage = styled.img`
-  object-fit: contain;
+  object-fit: cover;
+  object-position: center;
   border-radius: 50%;
 `;
