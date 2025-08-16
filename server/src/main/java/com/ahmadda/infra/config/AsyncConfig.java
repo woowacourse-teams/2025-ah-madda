494cdb7cc3d0cfa9bb288f145a7d4c153c644385
jeyong <joon6093@naver.com>
2025-08-16 14:14:17 +0900

    refactor: graceful shutdown 고려 필요 주석 추가

--- server/src/main/java/com/ahmadda/infra/config/AsyncConfig.java
+++ server/src/main/java/com/ahmadda/infra/config/AsyncConfig.java
@@ -1,7 +1,6 @@
 package com.ahmadda.infra.config;
 
 import org.slf4j.MDC;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
@@ -15,19 +14,12 @@ import java.util.concurrent.Executor;
 @EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
+    // TODO. 추후 ThreadPoolTaskExecutor의 thread 설정 변경 및 graceful shutdown 고려 필요
     @Override
     public Executor getAsyncExecutor() {
-        return taskExecutor();
-    }
-
-    // TODO. 추후 ThreadPoolTaskExecutor 설정 변경 필요
-    @Bean
-    public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setThreadNamePrefix("async-");
         executor.setTaskDecorator(new AsyncMDCTaskDecorator());
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.setAwaitTerminationSeconds(10);
         executor.initialize();
 
         return executor;
