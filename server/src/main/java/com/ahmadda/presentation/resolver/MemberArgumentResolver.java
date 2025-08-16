d76100983ebabddf278cd4c9ab097a2b77e9df4d
jumdo12 <jumdo12@gmail.com>
2025-08-16 14:36:06 +0900

    refactor: 코드 컨벤션 수정

--- server/src/main/java/com/ahmadda/presentation/resolver/MemberArgumentResolver.java
+++ server/src/main/java/com/ahmadda/presentation/resolver/MemberArgumentResolver.java
@@ -3,7 +3,7 @@ package com.ahmadda.presentation.resolver;
 import com.ahmadda.application.dto.LoginMember;
 import com.ahmadda.infra.login.jwt.JwtProvider;
 import com.ahmadda.infra.login.jwt.dto.JwtMemberPayload;
-import com.ahmadda.presentation.cookie.RefreshTokenCookieProvider;
+import com.ahmadda.presentation.exception.InvalidAuthorizationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
@@ -18,8 +18,9 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @RequiredArgsConstructor
 public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
 
+    private static final String BEARER_TYPE = "Bearer ";
+
     private final JwtProvider jwtProvider;
-    private final RefreshTokenCookieProvider refreshTokenCookieProvider;
 
     @Override
     public boolean supportsParameter(final MethodParameter parameter) {
@@ -36,11 +37,18 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
             final WebDataBinderFactory binderFactory
     ) {
         HttpServletRequest httpServletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        String accessToken =
-                refreshTokenCookieProvider.extractBearer(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION));
+        String accessToken = extractAccessToken(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION));
 
         JwtMemberPayload jwtMemberPayload = jwtProvider.parseAccessPayload(accessToken);
 
         return new LoginMember(jwtMemberPayload.getMemberId());
+    }
+
+    private String extractAccessToken(final String header) {
+        if (header != null && header.startsWith(BEARER_TYPE)) {
+            return header.substring(BEARER_TYPE.length())
+                    .trim();
+        }
+        throw new InvalidAuthorizationException("인증 토큰 정보가 존재하지 않거나 유효하지 않습니다.");
     }
 }
