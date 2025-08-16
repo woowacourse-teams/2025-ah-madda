8e8a69630401a25aea4332c784fc3742fca0ef05
LeeJaeHoon <dlwogns3413@naver.com>
2025-08-16 21:13:44 +0900

    refactor: 내 조직원 프로필 조회시 어드민 유무도 반환하게 변경

--- server/src/main/java/com/ahmadda/presentation/dto/OrganizationMemberResponse.java
+++ server/src/main/java/com/ahmadda/presentation/dto/OrganizationMemberResponse.java
@@ -1,16 +1,19 @@
 package com.ahmadda.presentation.dto;
 
 import com.ahmadda.domain.OrganizationMember;
+import com.ahmadda.domain.Role;
 
 public record OrganizationMemberResponse(
         Long organizationMemberId,
-        String nickname
+        String nickname,
+        boolean isAdmin
 ) {
 
     public static OrganizationMemberResponse from(final OrganizationMember organizationMember) {
         return new OrganizationMemberResponse(
                 organizationMember.getId(),
-                organizationMember.getNickname()
+                organizationMember.getNickname(),
+                organizationMember.getRole() == Role.ADMIN
         );
     }
 }
