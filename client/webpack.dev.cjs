70481ed0b16c7b24eabb831bc50c61a709a2c5f6
keemsebeen <sebin0580@gmail.com>
2025-08-19 15:51:26 +0900

    chore: webpack common, dev, prod 모드 변경

---
+++ client/webpack.dev.cjs
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const common = require('./webpack.common.cjs');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].js',
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, '.env.development'),
+      safe: false,
+      systemvars: true,
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true,
+    port: 5173,
+    hot: true,
+    open: true,
+  },
+});
