107d5d08b18df6884f9cdaf77e0e2ccc6cdb5944
keemsebeen <sebin0580@gmail.com>
2025-08-19 16:06:28 +0900

    chore: prod 난독화 추가

--- client/webpack.prod.cjs
+++ client/webpack.prod.cjs
@@ -1,7 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.cjs');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          mangle: true,
+        },
+      }),
+    ],
+  },
 });
