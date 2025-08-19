fe596fecf7405fa72fc3ad375f5d76172d34ebe2
keemsebeen <sebin0580@gmail.com>
2025-08-20 01:15:10 +0900

    feat: 로그인 하지 않은 사용자 Toast 연결

--- client/src/router/ProtectRoute.tsx
+++ client/src/router/ProtectRoute.tsx
@@ -4,15 +4,18 @@ import { Outlet, useNavigate } from 'react-router-dom';
 
 import { isAuthenticated } from '@/api/auth';
 
+import { useToast } from '../shared/components/Toast/ToastContext';
+
 export const ProtectRoute = () => {
   const navigate = useNavigate();
+  const { error } = useToast();
 
   useEffect(() => {
     if (!isAuthenticated()) {
-      alert('로그인이 필요한 서비스입니다. 먼저 로그인해 주세요.');
+      error('로그인이 필요한 서비스입니다. 먼저 로그인해 주세요.', { duration: 3000 });
       navigate('/');
     }
-  }, [navigate]);
+  }, [navigate, error]);
 
   if (!isAuthenticated()) {
     return null;
