55d86a27fd36c23ba63137b984f42847c75d0a6f
keemsebeen <sebin0580@gmail.com>
2025-08-20 16:21:07 +0900

    [FE] 사전 질문을 입력하지 않은 경우 버튼 disabled 처리 (#586)

--- client/src/features/Event/Detail/components/SubmitButtonCard.tsx
+++ client/src/features/Event/Detail/components/SubmitButtonCard.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+
 import { useCancelParticipation } from '@/api/mutations/useCancelParticipation';
 import { useParticipateEvent } from '@/api/mutations/useParticipateEvent';
 import { Answer, GuestStatusAPIResponse } from '@/api/types/event';
@@ -5,11 +7,14 @@ import { Button } from '@/shared/components/Button';
 import { Flex } from '@/shared/components/Flex';
 import { useToast } from '@/shared/components/Toast/ToastContext';
 
+import { getEventButtonState } from '../utils/getSubmitButtonState';
+
 type SubmitBUttonCardProps = {
   eventId: number;
   registrationEnd: string;
   answers: Answer[];
   onResetAnswers: VoidFunction;
+  isRequiredAnswerComplete: boolean;
 } & GuestStatusAPIResponse;
 
 export const SubmitButtonCard = ({
@@ -18,13 +23,18 @@ export const SubmitButtonCard = ({
   registrationEnd,
   isGuest,
   onResetAnswers,
+  isRequiredAnswerComplete,
 }: SubmitBUttonCardProps) => {
-  const now = new Date();
-  const { success, error } = useToast();
-  const isBeforeDeadline = now <= new Date(registrationEnd);
 
+  const { success, error } = useToast();
   const { mutate: participantMutate } = useParticipateEvent(eventId);
   const { mutate: cancelParticipateMutate } = useCancelParticipation(eventId);
+  
+  const buttonState = getEventButtonState({
+    registrationEnd,
+    isGuest,
+    isRequiredAnswerComplete,
+  });
 
   const handleParticipantClick = () => {
     participantMutate(answers, {
@@ -53,17 +63,16 @@ export const SubmitButtonCard = ({
     <Flex margin="10px 0 40px">
       <Button
         size="full"
-        color={!isGuest || isBeforeDeadline ? 'primary' : 'tertiary'}
-        disabled={!isBeforeDeadline}
-        onClick={isGuest ? handleCancelParticipateClick : handleParticipantClick}
+        color={buttonState.color}
+        disabled={buttonState.disabled}
+        onClick={
+          buttonState.action === 'cancel' ? handleCancelParticipateClick : handleParticipantClick
+        }
+        css={css`
+          transition: all 0.2s ease-in-out;
+        `}
       >
-        {isBeforeDeadline
-          ? isGuest
-            ? '신청 취소'
-            : '신청 하기'
-          : isGuest
-            ? '신청 완료'
-            : '신청 마감'}
+        {buttonState.text}
       </Button>
     </Flex>
   );
