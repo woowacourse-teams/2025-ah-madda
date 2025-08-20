f7132edf9afb110c17af51d0bbd70af5307254b9
장정안 <wjddks96@naver.com>
2025-08-20 21:52:30 +0900

    feat: ProtectRoute에 redirectAfterLogin을 로컬스토리지에 추가

--- client/src/router/ProtectRoute.tsx
+++ client/src/router/ProtectRoute.tsx
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
 
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
-import { isAuthenticated } from '@/api/auth';
+import { getGoogleAuthUrl, isAuthenticated } from '@/api/auth';
+import { setLocalStorage } from '@/shared/utils/localStorage';
 
 export const ProtectRoute = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     if (!isAuthenticated()) {
+      setLocalStorage('redirectAfterLogin', location.pathname);
       alert('로그인이 필요한 서비스입니다. 먼저 로그인해 주세요.');
-      navigate('/');
+      const authUrl = getGoogleAuthUrl();
+      window.location.href = authUrl;
     }
-  }, [navigate]);
+  }, [navigate, location.pathname]);
 
   if (!isAuthenticated()) {
     return null;
