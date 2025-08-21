9c62c8e505aec5399a08295b2f8832a522a6f9b9
장정안 <wjddks96@naver.com>
2025-08-21 20:54:59 +0900

    fix: 수용 인원 max 값 1억으로 설정

--- client/src/features/Event/New/hooks/useMaxCapacity.ts
+++ client/src/features/Event/New/hooks/useMaxCapacity.ts
@@ -2,6 +2,8 @@ import { ChangeEvent, useState } from 'react';
 
 import { useToast } from '@/shared/components/Toast/ToastContext';
 
+const MAX_CAPACITY = 100_000_000;
+
 export const useMaxCapacity = (initialValue: number | string) => {
   const { error } = useToast();
   const [maxCapacity, setMaxCapacity] = useState(initialValue);
@@ -12,6 +14,11 @@ export const useMaxCapacity = (initialValue: number | string) => {
     if (!Number.isInteger(numberValue)) {
       error('수용 인원은 숫자만 입력가능합니다.');
       setMaxCapacity(Number(maxCapacity));
+      return;
+    }
+
+    if (numberValue >= MAX_CAPACITY) {
+      error('수용 인원이 너무 큽니다. 더 작은 값을 입력해주세요.');
       return;
     }
 
