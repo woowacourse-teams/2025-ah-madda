fdcb4c9e3bf0b3e395c2fafbb760c5d45bfb2eeb
jeyong <joon6093@naver.com>
2025-08-21 13:21:57 +0900

    refacotr: 쿠키 설정 변경

--- server/src/main/java/com/ahmadda/presentation/cookie/config/RefreshTokenCookieProperties.java
+++ server/src/main/java/com/ahmadda/presentation/cookie/config/RefreshTokenCookieProperties.java
@@ -44,6 +44,10 @@ public class RefreshTokenCookieProperties {
             throw new IllegalArgumentException("쿠키 Path가 비어있습니다.");
         }
 
+        if (domain == null || domain.isBlank()) {
+            throw new IllegalArgumentException("쿠키 Domain이 비어있습니다.");
+        }
+
         if (sameSite == null || sameSite.isBlank()) {
             throw new IllegalArgumentException("쿠키 SameSite가 비어있습니다.");
         }
