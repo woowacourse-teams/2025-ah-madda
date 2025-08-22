6c3728574c3d0136d1fe458e5dfc35aa338d3d93
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2025-08-22 12:17:29 +0900

    [FE] 이벤트 생성페이지로 갈 때의 navigate 핫픽스 & 조직 생성하기 내 헤더의 '내 이벤트' 삭제 (#629)

--- client/src/features/Organization/New/pages/NewOrganizationPage.tsx
+++ client/src/features/Organization/New/pages/NewOrganizationPage.tsx
@@ -1,6 +1,5 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import { Button } from '@/shared/components/Button';
 import { Flex } from '@/shared/components/Flex';
 import { Header } from '@/shared/components/Header';
 import { IconButton } from '@/shared/components/IconButton';
@@ -10,25 +9,9 @@ import { OrganizationCreateForm } from '../components/OrganizationCreateForm';
 
 export const NewOrganizationPage = () => {
   const navigate = useNavigate();
-  const { organizationId } = useParams();
   return (
     <PageLayout
-      header={
-        <Header
-          left={
-            <IconButton
-              name="logo"
-              size={55}
-              onClick={() => navigate(`/${organizationId}/event`)}
-            />
-          }
-          right={
-            <Button size="sm" onClick={() => navigate(`/${organizationId}/event/my`)}>
-              내 이벤트
-            </Button>
-          }
-        />
-      }
+      header={<Header left={<IconButton name="logo" size={55} onClick={() => navigate(`/`)} />} />}
     >
       <Flex dir="column" width="100%" margin="0 auto" padding="28px 20px" gap="24px">
         <OrganizationCreateForm />
