3734323c97d6b4bdb3d27ade33eb271096ec0c37
jeyong <joon6093@naver.com>
2025-09-02 12:57:19 +0900

    [BE] 스케줄러 알림 저장 중 Hibernate 내부 상태 오류 해결 (#646)

--- server/src/main/java/com/ahmadda/infra/config/SchedulingConfig.java
+++ server/src/main/java/com/ahmadda/infra/config/SchedulingConfig.java
@@ -1,11 +1,27 @@
 package com.ahmadda.infra.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-// TODO. 추후 ThreadPoolTaskScheduler 설정 추가 필요
 @Configuration
 @EnableScheduling
-public class SchedulingConfig {
+public class SchedulingConfig implements SchedulingConfigurer {
 
+    @Override
+    public void configureTasks(final ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setTaskScheduler(taskScheduler());
+    }
+
+    // TODO. 추후 ThreadPoolTaskScheduler의 thread 설정 변경 및 graceful shutdown 고려 필요
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadNamePrefix("scheduler-");
+
+        return scheduler;
+    }
 }
