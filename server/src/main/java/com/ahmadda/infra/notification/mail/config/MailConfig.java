d52ff8cb87cc0d2e0b48eaa6c029d459710864fb
jeyong <joon6093@naver.com>
2025-09-10 16:19:09 +0900

    [BE] Amazon SES를 통한 이메일 발송 기능 전환 (#694)

--- server/src/main/java/com/ahmadda/infra/notification/mail/config/MailConfig.java
+++ server/src/main/java/com/ahmadda/infra/notification/mail/config/MailConfig.java
@@ -9,10 +9,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.thymeleaf.TemplateEngine;
 
-@EnableConfigurationProperties(NotificationProperties.class)
+@EnableConfigurationProperties({NotificationProperties.class, SmtpProperties.class})
 @Configuration
 public class MailConfig {
 
@@ -31,5 +33,36 @@ public class MailConfig {
             final EntityManager em
     ) {
         return new SmtpEmailNotifier(javaMailSender, templateEngine, notificationProperties, em);
+    }
+
+    @Bean
+    @Profile("prod")
+    public JavaMailSender awsMailSender(final SmtpProperties smtpProperties) {
+        SmtpProperties.Account account = smtpProperties.getAws();
+
+        return getJavaMailSender(account);
+    }
+
+    @Bean
+    @Profile("!prod")
+    public JavaMailSender gmailMailSender(final SmtpProperties smtpProperties) {
+        SmtpProperties.Account account = smtpProperties.getGoogle();
+
+        return getJavaMailSender(account);
+    }
+
+    private JavaMailSender getJavaMailSender(final SmtpProperties.Account acc) {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(acc.getHost());
+        sender.setPort(acc.getPort());
+        sender.setUsername(acc.getUsername());
+        sender.setPassword(acc.getPassword());
+        sender.setDefaultEncoding("UTF-8");
+        if (acc.getProperties() != null) {
+            sender.getJavaMailProperties()
+                    .putAll(acc.getProperties());
+        }
+
+        return sender;
     }
 }
