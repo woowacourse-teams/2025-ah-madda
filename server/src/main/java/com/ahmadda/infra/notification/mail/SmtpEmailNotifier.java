6ea493acb9956c45601119b52ad674a42039ce14
jeyong <joon6093@naver.com>
2025-09-10 14:13:39 +0900

    fix: aws ses 554 Message rejected: Email address is not verified 문제 해결

--- server/src/main/java/com/ahmadda/infra/notification/mail/SmtpEmailNotifier.java
+++ server/src/main/java/com/ahmadda/infra/notification/mail/SmtpEmailNotifier.java
@@ -101,7 +101,7 @@ public class SmtpEmailNotifier implements EmailNotifier {
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
-            helper.setTo("amadda.team@gmail.com");
+            helper.setFrom("아맞다 <noreply@ahmadda.com>");
             // TODO. 추후 BCC 수신자가 100명 이상일 경우, 배치 처리 고려
             helper.setBcc(bccRecipients.toArray(String[]::new));
             helper.setSubject(subject);
