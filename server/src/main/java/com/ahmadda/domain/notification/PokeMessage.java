4de8e6af379d21e8948ed833e12c10a3b8defd91
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-12 15:20:09 +0900

    [BE] 포키 메시지 형식 4가지로 지정 (#713)

---
+++ server/src/main/java/com/ahmadda/domain/notification/PokeMessage.java
@@ -0,0 +1,18 @@
+package com.ahmadda.domain.notification;
+
+public enum PokeMessage {
+    RECOMMEND("%s님이 이벤트 참여를 추천했어요! 🌈"),
+    WAITING("%s님이 참여를 기다려요 ⏰"),
+    ARRIVED("%s님의 포키가 도착했어요! ✨"),
+    HEART("%s님이 당신을 포키했어요! ❤️");
+
+    private final String messageFormat;
+
+    PokeMessage(final String messageFormat) {
+        this.messageFormat = messageFormat;
+    }
+
+    public String getMessage(final String senderNickname) {
+        return String.format(messageFormat, senderNickname);
+    }
+}
