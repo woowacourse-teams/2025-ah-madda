9ce47b1fe294074410b3bb9112c09fc8ac40f78d
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-16 20:05:21 +0900

    cicd: 실행파일 기준 경로 적용

--- codedeploy/be/scripts/start.sh
+++ codedeploy/be/scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # scripts/start.sh
 
-APP_DIR="/home/ubuntu/app"
+APP_DIR="/home/ubuntu/app/"
 JAR_FILE="ahmadda-server-prod.jar"
 LOG_FILE="/home/ubuntu/app/app.log"
 PROFILE="prod"
 
 echo "애플리케이션을 시작합니다..."
-sudo java -Dspring.profiles.active=$PROFILE -jar $JAR_FILE
+sudo java -Dspring.profiles.active=$PROFILE -jar $APP_DIR$JAR_FILE
