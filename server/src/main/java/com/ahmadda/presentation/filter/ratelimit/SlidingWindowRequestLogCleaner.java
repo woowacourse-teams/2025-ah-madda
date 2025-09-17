9b19e61a11b927a30dfd61d87dbba9f581b030dc
jumdo12 <jumdo12@gmail.com>
2025-09-17 10:44:55 +0900

    feat: scheduleLock 추가

--- server/src/main/java/com/ahmadda/presentation/filter/ratelimit/SlidingWindowRequestLogCleaner.java
+++ server/src/main/java/com/ahmadda/presentation/filter/ratelimit/SlidingWindowRequestLogCleaner.java
@@ -1,6 +1,7 @@
 package com.ahmadda.presentation.filter.ratelimit;
 
 import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,11 @@ public class SlidingWindowRequestLogCleaner {
     private final SlidingWindowRateLimitFilter rateLimitFilter;
 
     @Scheduled(fixedDelay = 10 * 60 * 1000)
+    @SchedulerLock(
+            name = "cleanUpStaleRequestLogs",
+            lockAtMostFor = "2m",
+            lockAtLeastFor = "10s"
+    )
     public void cleanUpStaleRequestLogs() {
         rateLimitFilter.cleanUpStaleRequestLogsInternal();
     }
