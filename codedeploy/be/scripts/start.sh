d79767f3d99765dedd47085cdaa6fdc8e3fd3026
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-18 18:18:36 +0900

    cicd: 로그 확인을 위해 설정 변경

--- codedeploy/be/scripts/start.sh
+++ codedeploy/be/scripts/start.sh
@@ -3,8 +3,7 @@
 
 APP_DIR="/home/ubuntu/app/"
 JAR_FILE="ahmadda-server-prod.jar"
-LOG_FILE="/home/ubuntu/app/app.log"
 PROFILE="prod"
 
 echo "애플리케이션을 시작합니다..."
-nohup sudo java -Dspring.profiles.active=$PROFILE -jar $APP_DIR$JAR_FILE > $LOG_FILE 2>&1 &
+nohup sudo java -Dspring.profiles.active=$PROFILE -jar $APP_DIR$JAR_FILE &
