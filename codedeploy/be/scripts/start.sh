fde18c86fc591dc0f651fca47ace2493877dc000
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-09-18 19:57:22 +0900

    refactor: shedlock 테이블까지 필요하기에 pyton 테스트용 서버방식으로 전환

--- codedeploy/be/scripts/start.sh
+++ codedeploy/be/scripts/start.sh
@@ -5,5 +5,6 @@ APP_DIR="/home/ubuntu/app/"
 JAR_FILE="ahmadda-server-prod.jar"
 PROFILE="prod"
 
-echo "애플리케이션을 시작합니다..."
-nohup sudo java -Dspring.profiles.active=$PROFILE -jar $APP_DIR$JAR_FILE &
+echo "테스트 애플리케이션을 시작합니다..."
+
+python3 -m http.server 8080 &
