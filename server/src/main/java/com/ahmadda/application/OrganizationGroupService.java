c72056fa82d49837f5115e04a0c0514456829ba2
LeeJaeHoon <dlwogns3413@naver.com>
2025-09-23 02:22:17 +0900

    feat: 조직 전체 조회 api 구현

--- server/src/main/java/com/ahmadda/application/OrganizationGroupService.java
+++ server/src/main/java/com/ahmadda/application/OrganizationGroupService.java
@@ -1,10 +1,19 @@
 package com.ahmadda.application;
 
+import com.ahmadda.domain.organization.OrganizationGroup;
+import com.ahmadda.domain.organization.OrganizationGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrganizationGroupService {
 
+    private final OrganizationGroupRepository organizationGroupRepository;
+
+    public List<OrganizationGroup> findAll() {
+        return organizationGroupRepository.findAll();
+    }
 }
---
+++ server/src/test/java/com/ahmadda/application/OrganizationGroupServiceTest.java
@@ -0,0 +1,39 @@
+package com.ahmadda.application;
+
+import com.ahmadda.annotation.IntegrationTest;
+import com.ahmadda.domain.organization.OrganizationGroup;
+import com.ahmadda.domain.organization.OrganizationGroupRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IntegrationTest
+class OrganizationGroupServiceTest {
+
+    @Autowired
+    private OrganizationGroupService sut;
+
+    @Autowired
+    private OrganizationGroupRepository organizationGroupRepository;
+
+    @Test
+    void 모든_그룹을_조회할_수_있다() {
+        //given
+        var group1 = createGroup("코치");
+        var group2 = createGroup("백엔드");
+        var group3 = createGroup("프론트");
+
+        //when
+        var organizationGroups = sut.findAll();
+
+        //then
+        assertThat(organizationGroups).isEqualTo(List.of(group1, group2, group3));
+    }
+
+    private OrganizationGroup createGroup(String name) {
+        return organizationGroupRepository.save(OrganizationGroup.create(name));
+    }
+}
