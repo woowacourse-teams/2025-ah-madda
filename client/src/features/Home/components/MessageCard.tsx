128da871f94b26bb7cb4099c59f28d1fe20d23d0
keemsebeen <sebin0580@gmail.com>
2025-09-22 21:55:03 +0900

    feat: 이미지 태그에 width, height 적용, loading 속성 적용

--- client/src/features/Home/components/MessageCard.tsx
+++ client/src/features/Home/components/MessageCard.tsx
@@ -5,10 +5,12 @@ import { Flex } from '@/shared/components/Flex';
 import { Text } from '@/shared/components/Text';
 
 type MessageCardProps = {
+  index: number;
   message: string;
   image: string;
 };
-export const MessageCard = ({ message, image }: MessageCardProps) => {
+
+export const MessageCard = ({ message, image, index }: MessageCardProps) => {
   return (
     <Flex
       dir="column"
@@ -23,14 +25,23 @@ export const MessageCard = ({ message, image }: MessageCardProps) => {
       <Text type="Heading" weight="semibold">
         {message}
       </Text>
-      <LandingImage src={image} alt={image} />
+      <LandingImage
+        src={image}
+        alt={image}
+        width={600}
+        height={300}
+        loading={index > 1 ? 'lazy' : 'eager'}
+        sizes="(max-width: 768px) 50vw, 60vw"
+      />
     </Flex>
   );
 };
+
 const LandingImage = styled.img`
   width: 60%;
   height: auto;
   align-self: flex-end;
+  aspect-ratio: 3 / 2;
 
   @media (max-width: 768px) {
     width: 50%;
