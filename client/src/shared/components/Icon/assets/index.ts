41a2bcb30b46d5492295bd755ff54e9b954bcf56
keemsebeen <sebin0580@gmail.com>
2025-09-23 01:22:17 +0900

    [FE] 스페이스원이 아닌 경우 스페이스 참여 링크로 유도 (#760)

--- client/src/shared/components/Icon/assets/index.ts
+++ client/src/shared/components/Icon/assets/index.ts
@@ -7,6 +7,7 @@ import { Close } from './close';
 import { Delete } from './delete';
 import { DropdownDown } from './dropdownDown';
 import { DropdownUp } from './dropdownUp';
+import { Edit } from './edit';
 import { Location } from './location';
 import { Logo } from './logo';
 import { Next } from './next';
@@ -23,6 +24,7 @@ export const Icons = {
   clock: Clock,
   close: Close,
   delete: Delete,
+  edit: Edit,
   location: Location,
   logo: Logo,
   plus: Plus,
