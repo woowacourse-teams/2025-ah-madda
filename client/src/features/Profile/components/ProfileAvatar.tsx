50b4c6d788c263bb23bc36de3273682d891a252f
keemsebeen <sebin0580@gmail.com>
2025-09-23 19:36:03 +0900

    feat: ProfileAvatar 컴포넌트에 사용자 email도 함께 노출

--- client/src/features/Profile/components/ProfileAvatar.tsx
+++ client/src/features/Profile/components/ProfileAvatar.tsx
@@ -1,28 +1,22 @@
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { Flex } from '@/shared/components/Flex';
+import { Text } from '@/shared/components/Text';
 import { DEFAULT_AVATAR_URL } from '@/shared/constants';
 
 type ProfileAvatarProps = {
+  email: string;
   src?: string;
   alt?: string;
 };
 
-export const ProfileAvatar = ({ src, alt = '프로필 이미지' }: ProfileAvatarProps) => {
+export const ProfileAvatar = ({ email, src, alt = '프로필 이미지' }: ProfileAvatarProps) => {
   return (
-    <Flex
-      dir="column"
-      gap="16px"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-      css={css`
-        border-radius: 50%;
-        overflow: hidden;
-      `}
-    >
-      <ProfileImage src={src || DEFAULT_AVATAR_URL} alt={alt} onError={() => {}} />
+    <Flex dir="column" gap="16px" justifyContent="center" alignItems="center" width="100%">
+      <ProfileImage src={src || DEFAULT_AVATAR_URL} alt={alt} width={80} height={80} />
+      <Text type="Body" weight="semibold" color="gray900">
+        {email}
+      </Text>
     </Flex>
   );
 };
