eaee5df4006f048a0a9e7092640c6f27b214bfa6
jeyong <joon6093@naver.com>
2025-10-01 17:43:58 +0900

    feat: Writer/Reader DataSource 분리 설정 추가

---
+++ server/src/main/java/com/ahmadda/infra/database/DataSourceConfig.java
@@ -0,0 +1,57 @@
+package com.ahmadda.infra.database;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.writer")
+    @ConditionalOnProperty(
+            prefix = "spring.datasource.replication",
+            name = "enabled",
+            havingValue = "true"
+    )
+    public DataSource writerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.reader")
+    @ConditionalOnProperty(
+            prefix = "spring.datasource.replication",
+            name = "enabled",
+            havingValue = "true"
+    )
+    public DataSource readerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(
+            prefix = "spring.datasource.replication",
+            name = "enabled",
+            havingValue = "true"
+    )
+    public DataSource routingDataSource(
+            @Qualifier("writerDataSource") DataSource writer,
+            @Qualifier("readerDataSource") DataSource reader
+    ) {
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put("writer", writer);
+        targetDataSources.put("reader", reader);
+
+        ReplicationRoutingDataSource routing = new ReplicationRoutingDataSource();
+        routing.setDefaultTargetDataSource(writer);
+        routing.setTargetDataSources(targetDataSources);
+        return routing;
+    }
+}
