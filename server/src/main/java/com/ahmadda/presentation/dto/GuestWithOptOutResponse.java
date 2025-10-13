e010f573a7553b3e06cb7f2d2ba26740d7141b50
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-10-13 19:37:05 +0900

    feat: 게스트 반환시 그룹 정보 추가

--- server/src/main/java/com/ahmadda/presentation/dto/GuestWithOptOutResponse.java
+++ server/src/main/java/com/ahmadda/presentation/dto/GuestWithOptOutResponse.java
@@ -1,15 +1,21 @@
 package com.ahmadda.presentation.dto;
 
 import com.ahmadda.domain.event.GuestWithOptStatus;
+import com.ahmadda.domain.organization.OrganizationGroup;
 
 public record GuestWithOptOutResponse(
         Long guestId,
         Long organizationMemberId,
         String nickname,
-        boolean optedOut
+        boolean optedOut,
+        String groupName,
+        Long groupId
 ) {
 
     public static GuestWithOptOutResponse from(final GuestWithOptStatus guestWithOptStatus) {
+        OrganizationGroup group = guestWithOptStatus.getOrganizationMember()
+                .getGroup();
+        
         return new GuestWithOptOutResponse(
                 guestWithOptStatus.getGuest()
                         .getId(),
@@ -19,7 +25,9 @@ public record GuestWithOptOutResponse(
                 guestWithOptStatus.getGuest()
                         .getOrganizationMember()
                         .getNickname(),
-                guestWithOptStatus.isOptedOut()
+                guestWithOptStatus.isOptedOut(),
+                group.getName(),
+                group.getId()
         );
     }
 }
--- server/src/main/java/com/ahmadda/presentation/dto/OrganizationMemberWithOptOutResponse.java
+++ server/src/main/java/com/ahmadda/presentation/dto/OrganizationMemberWithOptOutResponse.java
@@ -1,20 +1,28 @@
 package com.ahmadda.presentation.dto;
 
+import com.ahmadda.domain.organization.OrganizationGroup;
 import com.ahmadda.domain.organization.OrganizationMemberWithOptStatus;
 
 public record OrganizationMemberWithOptOutResponse(
         Long organizationMemberId,
         String nickname,
-        boolean optedOut
+        boolean optedOut,
+        String groupName,
+        Long groupId
 ) {
 
     public static OrganizationMemberWithOptOutResponse from(final OrganizationMemberWithOptStatus organizationMemberWithOptStatus) {
+        OrganizationGroup group = organizationMemberWithOptStatus.getOrganizationMember()
+                .getGroup();
+
         return new OrganizationMemberWithOptOutResponse(
                 organizationMemberWithOptStatus.getOrganizationMember()
                         .getId(),
                 organizationMemberWithOptStatus.getOrganizationMember()
                         .getNickname(),
-                organizationMemberWithOptStatus.isOptedOut()
+                organizationMemberWithOptStatus.isOptedOut(),
+                group.getName(),
+                group.getId()
         );
     }
 }
