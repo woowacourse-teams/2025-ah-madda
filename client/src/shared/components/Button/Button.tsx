8fa7322b61dbb57719c52ff902046442107030a1
keemsebeen <sebin0580@gmail.com>
2025-10-18 22:45:31 +0900

    feat: Button props에 isLoading 속성 추가

--- client/src/shared/components/Button/Button.tsx
+++ client/src/shared/components/Button/Button.tsx
@@ -2,11 +2,12 @@ import { ComponentPropsWithRef, PropsWithChildren } from 'react';
 
 import { Icon } from '../Icon';
 import { IconName } from '../Icon/assets';
+import { Loading } from '../Loading';
 
 import { StyledButton } from './Button.styled';
 
-type Variant = 'solid' | 'outline';
-type Color = 'primary' | 'secondary' | 'tertiary';
+type Variant = 'solid' | 'ghost' | 'outline';
+export type Color = 'primary' | 'secondary' | 'tertiary';
 export type ButtonProps = {
   /**
    * The size of the button.
@@ -38,6 +39,12 @@ export type ButtonProps = {
    * @default 'button'
    */
   type?: 'button' | 'submit' | 'reset';
+  /**
+   * Whether the button is loading.
+   * @type {boolean}
+   * @default false
+   */
+  isLoading?: boolean;
 } & PropsWithChildren<ComponentPropsWithRef<'button'>>;
 
 // S.TODO: isLoading 추가, 아이콘이 들어가는 경우
@@ -48,6 +55,7 @@ export const Button = ({
   iconName,
   type = 'button',
   children,
+  isLoading = false,
   ...props
 }: ButtonProps) => {
   return (
@@ -59,6 +67,14 @@ export const Button = ({
       iconName={iconName}
       {...props}
     >
+      {isLoading && (
+        <Loading
+          type="spinner"
+          size={size === 'sm' ? 14 : size === 'md' ? 16 : 20}
+          color={variant === 'outline' ? 'gray' : color === 'secondary' ? 'primary' : 'white'}
+        />
+      )}
+
       {size === 'md' && iconName && (
         <Icon
           name={iconName}
