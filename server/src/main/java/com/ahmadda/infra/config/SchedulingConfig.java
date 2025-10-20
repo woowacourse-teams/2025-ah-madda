18448437b284e7b7b202e35a0bc44aece710c80a
jeyong <joon6093@naver.com>
2025-10-20 18:50:35 +0900

    fix: 테스트에서 scheduling 문제 해결

--- server/src/main/java/com/ahmadda/infra/config/SchedulingConfig.java
+++ server/src/main/java/com/ahmadda/infra/config/SchedulingConfig.java
@@ -3,6 +3,7 @@ package com.ahmadda.infra.config;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,6 +15,12 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import javax.sql.DataSource;
 
 @Configuration
+@ConditionalOnProperty(
+        prefix = "spring.scheduling",
+        name = "enabled",
+        havingValue = "true",
+        matchIfMissing = true
+)
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "5m")
 public class SchedulingConfig implements SchedulingConfigurer {
