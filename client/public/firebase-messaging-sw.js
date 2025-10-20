e12d8da2ca329a90bb4d21d09b206737f0448edf
keemsebeen <sebin0580@gmail.com>
2025-10-20 14:46:25 +0900

    [FE] HomePage 3d 이미지 적용 (#897)

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -23,7 +23,7 @@ messaging.onBackgroundMessage((payload) => {
 
   const notificationOptions = {
     body: payload.notification?.body || '내용 없음',
-    icon: '/icon-512x512.png',
+    icon: '/icon-192x192.png',
     data: payload.data,
     tag: payload.data.eventId || 'default',
     requireInteraction: true,
