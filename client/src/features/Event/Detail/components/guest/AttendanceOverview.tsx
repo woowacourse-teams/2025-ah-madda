12e3d472454813a40fcc55fbf88544b90d5afb6f
keemsebeen <sebin0580@gmail.com>
2025-10-22 01:46:45 +0900

    feat: Tooltip 연결

--- client/src/features/Event/Detail/components/guest/AttendanceOverview.tsx
+++ client/src/features/Event/Detail/components/guest/AttendanceOverview.tsx
@@ -1,9 +1,13 @@
+import { css } from '@emotion/react';
 import { useSuspenseQueries, useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
 import { eventQueryOptions } from '@/api/queries/event';
 import { organizationQueryOptions } from '@/api/queries/organization';
 import { Flex } from '@/shared/components/Flex';
+import { Icon } from '@/shared/components/Icon';
+import { Text } from '@/shared/components/Text';
+import { Tooltip } from '@/shared/components/Tooltip/Tooltip';
 import { theme } from '@/shared/styles/theme';
 
 import { GuestList } from './GuestList';
@@ -32,6 +36,14 @@ export const AttendanceOverview = ({ eventId }: { eventId: number }) => {
 
   return (
     <Flex as="section" width="100%" dir="column">
+      <Flex justifyContent="flex-end" padding="12px 0 0 0">
+        <Tooltip
+          placement="left-bottom"
+          content={`포키 알림은 로그인 후 스페이스에 참여한 친구에게만 보낼 수 있어요.\n친구를 클릭해 알림을 보내보세요!`}
+        >
+          <Icon name="info" color="gray500" size={28} />
+        </Tooltip>
+      </Flex>
       <GuestList
         eventId={eventId}
         title={`신청 완료 (${guests.length}명)`}
