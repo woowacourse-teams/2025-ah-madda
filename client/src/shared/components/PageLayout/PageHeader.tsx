15960fcfccf34f51467ea05821171836c2a775aa
keemsebeen <sebin0580@gmail.com>
2025-10-23 15:57:36 +0900

    feat: 헤더에 로그인 모달 추가

--- client/src/shared/components/PageLayout/PageHeader.tsx
+++ client/src/shared/components/PageLayout/PageHeader.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 
 import { getGoogleAuthUrl, isAuthenticated } from '@/api/auth';
+import { useModal } from '@/shared/hooks/useModal';
 
 import { Button } from '../Button';
 import { Flex } from '../Flex';
@@ -9,8 +10,11 @@ import { Header } from '../Header';
 import { Icon } from '../Icon';
 import { IconButton } from '../IconButton';
 
+import { LoginInfoModal } from './LoginInfoModal';
+
 export const PageHeader = () => {
   const navigate = useNavigate();
+  const { isOpen, open, close } = useModal();
 
   const goHome = () => navigate(`/`);
   const goMyEvents = () => navigate(`/my`);
@@ -23,33 +27,37 @@ export const PageHeader = () => {
   };
 
   return (
-    <Header
-      left={
-        <Icon
-          name="logo"
-          size={55}
-          onClick={goHome}
-          css={css`
-            cursor: pointer;
-          `}
-        />
-      }
-      right={
-        isAuthenticated() ? (
-          <Flex alignItems="center" gap="8px">
-            <Button size="sm" onClick={goMyEvents}>
-              내 이벤트
-            </Button>
-            <IconButton name="user" size={24} onClick={goProfile} />
-          </Flex>
-        ) : (
-          <>
-            <Button size="sm" onClick={handleLogin}>
-              로그인
-            </Button>
-          </>
-        )
-      }
-    />
+    <>
+      <Header
+        left={
+          <Icon
+            name="logo"
+            size={55}
+            onClick={goHome}
+            css={css`
+              cursor: pointer;
+            `}
+          />
+        }
+        right={
+          isAuthenticated() ? (
+            <Flex alignItems="center" gap="8px">
+              <Button size="sm" onClick={goMyEvents}>
+                내 이벤트
+              </Button>
+              <IconButton name="user" size={24} onClick={goProfile} />
+            </Flex>
+          ) : (
+            <Flex alignItems="center" gap="8px">
+              <Button size="sm" onClick={handleLogin}>
+                로그인
+              </Button>
+              <IconButton name="queueMicrotask" size={24} color="gray500" onClick={open} />
+            </Flex>
+          )
+        }
+      />
+      <LoginInfoModal isOpen={isOpen} onClose={close} />
+    </>
   );
 };
