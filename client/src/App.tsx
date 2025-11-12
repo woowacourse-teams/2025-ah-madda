94cd611a1c1ab75a1ace51598e1557ee8cba0e1a
keemsebeen <sebin0580@gmail.com>
2025-11-13 01:25:37 +0900

    [FE] 채널톡 io 설치 (#984)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import { Outlet } from 'react-router-dom';
 import { ErrorPage } from '@/features/Error/pages/ErrorPage';
 
 import { ToastProvider } from './shared/components/Toast/ToastContext';
+import { useChannelTalk } from './shared/hooks/useChannelTalk';
 import { usePageTrack } from './shared/hooks/usePageTrack';
 import { useInitializeFCM } from './shared/notification/useInitializeFCM';
 
 export const App = () => {
   usePageTrack();
   useInitializeFCM();
+  useChannelTalk();
 
   return (
     <Sentry.ErrorBoundary fallback={<ErrorPage />}>
